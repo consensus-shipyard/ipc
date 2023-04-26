3586fe124d50a4bcad1c4c67d6b84794f9d3addb
Akosh Farkash <aakoshh@gmail.com>
2023-04-26 12:46:36 +0100

    IPC-91: Test env (Part 1) (#157)
    * IPC-91: Fix infra directory mkdir. Exit on fail.
    
    * IPC-91: Update init config command
    
    * IPC-91: Makefile to build the agent and eudico
    
    * IPC-91: Allow empty subnets list
    
    * IPC-91: Create agent docker-compose, up and down
    
    * IPC-91: make clean
    
    * IPC-91: Trying to get a eudico node going
    
    * IPC-91: Separate daemon and validator
    
    * IPC-91: Docs and clean
    
    * IPC-91: Fix API address for the validator
    
    * IPC-91: Remove volumes
    
    * IPC-91: Copy the infra scripts that needed modification.
    
    * IPC-91: Connect script. Fix clean
    
    * IPC-91: Try reload the agent config
    
    * IPC-91: Fix host to accept connections
    
    * IPC-91: Sleep after wait. Fix finding files and config when no subnets yet
    
    * IPC-91: Tab to spaces
    
    * IPC-91: Spaces to tabs
    
    * IPC-91: Config not needed to call reload

--- scripts/install_infra.sh
+++ scripts/install_infra.sh
@@ -2,12 +2,14 @@
 #
 # Builds docker image and install the ipc-scripts required to conveniently
 # deploy the infrastructure for IPC subnets.
+
+set -e
+
 rm -rf ./lotus
 git clone https://github.com/consensus-shipyard/lotus.git
 cd ./lotus
 docker build -t eudico .
 cd ..
-mkdir -p ./bin
 mkdir -p ./bin/ipc-infra
 cp -rf ./lotus/scripts/ipc/* ./bin/ipc-infra
 rm -rf ./lotus
