87ba7ca49440aa404de85df61f9403a3a49312ae
Denis Kolegov <dnkolegov@gmail.com>
2023-08-03 16:36:44 +0300

    Add licenses (#175)
    * Add licenses
    
    * Fix license combination
    
    ---------
    
    Co-authored-by: Jorge Soares <547492+jsoares@users.noreply.github.com>

--- src/lib/CrossMsgHelper.sol
+++ src/lib/CrossMsgHelper.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity 0.8.19;
 
 import {METHOD_SEND, EMPTY_BYTES, BURNT_FUNDS_ACTOR} from "../constants/Constants.sol";
