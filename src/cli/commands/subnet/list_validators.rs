2b057da041fc3d53ae0c5d97cab82e4996f19f90
Denis Kolegov <dnkolegov@gmail.com>
2023-08-12 14:20:14 +0300

    add list validators

---
+++ src/cli/commands/subnet/list_validators.rs
@@ -0,0 +1,63 @@
+// Copyright 2022-2023 Protocol Labs
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: MIT
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: MIT
+//! Get membership cli command
+
+use async_trait::async_trait;
+use clap::Args;
+use std::fmt::Debug;
+
+use crate::cli::commands::get_ipc_agent_url;
+use crate::cli::{CommandLineHandler, GlobalArguments};
+use crate::config::json_rpc_methods;
+use crate::jsonrpc::{JsonRpcClient, JsonRpcClientImpl};
+use crate::lotus::message::ipc::QueryValidatorSetResponse;
+use crate::server::query_validators::QueryValidatorSetParams;
+use serde::Deserialize;
+
+/// The command to create a new subnet actor.
+pub(crate) struct ListValidators;
+
+#[async_trait]
+impl CommandLineHandler for ListValidators {
+    type Arguments = ListValidatorsArgs;
+
+    async fn handle(global: &GlobalArguments, arguments: &Self::Arguments) -> anyhow::Result<()> {
+        log::debug!("list validators with args: {:?}", arguments);
+
+        let url = get_ipc_agent_url(&arguments.ipc_agent_url, global)?;
+        let json_rpc_client = JsonRpcClientImpl::new(url, None);
+
+        let params = QueryValidatorSetParams {
+            gateway_address: arguments.gateway_address.clone(),
+            subnet_id: arguments.subnet.clone(),
+        };
+
+        let valset = json_rpc_client
+            .request::<QueryValidatorSetResponse>(
+                json_rpc_methods::QUERY_VALIDATOR_SET,
+                serde_json::to_value(params)?,
+            )
+            .await?;
+
+        log::info!("validators number: {}", valset.min_validators);
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Args)]
+#[command(
+    name = "membership",
+    about = "Show the current membership of the subnet"
+)]
+pub(crate) struct ListValidatorsArgs {
+    #[arg(long, short, help = "The JSON RPC server url for ipc agent")]
+    pub ipc_agent_url: Option<String>,
+    #[arg(long, short, help = "The gateway address to query subnets")]
+    pub gateway_address: String,
+    #[arg(long, short, help = "The subnet id to query validators")]
+    pub subnet: String,
+}
---
+++ src/server/handlers/manager/query_validators.rs
@@ -0,0 +1,62 @@
+// Copyright 2022-2023 Protocol Labs
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: MIT
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: MIT
+//! Query the validator set in subnet actor
+
+use crate::lotus::message::ipc::QueryValidatorSetResponse;
+
+use crate::server::handlers::manager::check_subnet;
+use crate::server::handlers::manager::subnet::SubnetManagerPool;
+use crate::server::JsonRPCRequestHandler;
+use anyhow::anyhow;
+use async_trait::async_trait;
+use fvm_shared::address::Address;
+use ipc_sdk::subnet_id::SubnetID;
+use serde::de::Unexpected::Option;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use std::sync::Arc;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueryValidatorSetParams {
+    pub gateway_address: String,
+    pub subnet_id: String,
+}
+
+/// The list validators json rpc method handler.
+pub(crate) struct QueryValidatorSetHandler {
+    pool: Arc<SubnetManagerPool>,
+}
+
+impl QueryValidatorSetHandler {
+    pub(crate) fn new(pool: Arc<SubnetManagerPool>) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait]
+impl JsonRPCRequestHandler for QueryValidatorSetHandler {
+    type Request = QueryValidatorSetParams;
+    type Response = QueryValidatorSetResponse;
+
+    async fn handle(&self, request: Self::Request) -> anyhow::Result<Self::Response> {
+        let subnet = SubnetID::from_str(&request.subnet_id)?;
+        let conn = match self.pool.get(&subnet) {
+            None => return Err(anyhow!("target parent subnet not found")),
+            Some(conn) => conn,
+        };
+
+        let subnet_config = conn.subnet();
+        check_subnet(subnet_config)?;
+
+        let gateway_addr = Address::from_str(&request.gateway_address);
+        let val_set = conn
+            .manager()
+            .get_validator_set(&subnet, gateway_addr.ok())
+            .await?;
+        log::debug!("list of validators: {val_set:?}");
+        Ok(val_set)
+    }
+}
