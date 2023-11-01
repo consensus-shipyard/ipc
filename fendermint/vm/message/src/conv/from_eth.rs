c4edb83c46263cece3602da5a4ecaef666e161b0
cryptoAtwill <108330426+cryptoAtwill@users.noreply.github.com>
2023-11-01 21:24:45 +0800

    Fill sender if it's None (#375)
    * fill from
    
    * trim syntax

--- fendermint/vm/message/src/conv/from_eth.rs
+++ fendermint/vm/message/src/conv/from_eth.rs
@@ -6,7 +6,7 @@
 use ethers_core::types::{Eip1559TransactionRequest, NameOrAddress, H160, U256};
 use fendermint_vm_actor_interface::{
     eam::{self, EthAddress},
-    evm,
+    evm, system,
 };
 use fvm_ipld_encoding::{BytesSer, RawBytes};
 use fvm_shared::{
@@ -37,7 +37,8 @@ pub fn to_fvm_message(tx: &Eip1559TransactionRequest) -> anyhow::Result<Message>
     // The `from` of the transaction is inferred from the signature.
     // As long as the client and the server use the same hashing scheme,
     // this should be usable as a delegated address.
-    let from = to_fvm_address(tx.from.unwrap_or_default());
+    // If none, use SYSTEM_ACTOR_ADDR. This is similar to https://github.com/filecoin-project/lotus/blob/master/node/impl/full/eth_utils.go#L124
+    let from = tx.from.map_or(system::SYSTEM_ACTOR_ADDR, to_fvm_address);
 
     // Wrap calldata in IPLD byte format.
     let calldata = tx.data.clone().unwrap_or_default().to_vec();
