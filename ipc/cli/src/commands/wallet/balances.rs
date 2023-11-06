de0ea1f5e65233c9d2f938b4cc00f8db4341bb2a
Alfonso de la Rocha <adlrocha@tutamail.com>
2023-11-06 11:42:07 +0100

    IPC-373: Fix balances returned with bogus rpc

--- ipc/cli/src/commands/wallet/balances.rs
+++ ipc/cli/src/commands/wallet/balances.rs
@@ -30,7 +30,6 @@ impl CommandLineHandler for WalletBalances {
             WalletType::Evm => {
                 let wallet = provider.evm_wallet()?;
                 let addresses = wallet.read().unwrap().list()?;
-                let mut no_balance = addresses.clone();
                 let r = addresses
                     .iter()
                     .map(|addr| {
@@ -50,15 +49,18 @@ impl CommandLineHandler for WalletBalances {
 
                 let v: Vec<anyhow::Result<(TokenAmount, &EthKeyAddress)>> = join_all(r).await;
 
-                for r in v.into_iter().filter_map(|r| r.ok()) {
-                    let (balance, addr) = r;
-                    if addr.to_string() != "default-key" {
-                        println!("{} - Balance: {}", addr.to_string(), balance);
-                        no_balance.retain(|a| a != addr);
+                for r in v.into_iter() {
+                    match r {
+                        Ok(i) => {
+                            let (balance, addr) = i;
+                            if addr.to_string() != "default-key" {
+                                println!("{} - Balance: {}", addr.to_string(), balance);
+                            }
+                        }
+                        Err(e) => {
+                            return Err(e);
+                        }
                     }
-                }
-                for addr in no_balance {
-                    println!("{} - Balance: 0", addr.to_string());
                 }
             }
             WalletType::Fvm => {
