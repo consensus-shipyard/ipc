949c4672ee955d758fef23bb42f2f058fd1dad12
raulk <raul@protocol.ai>
2023-12-15 21:15:59 +0300

    cleanup: remove unused `AccountHelper#isAccount` predicate. (#354)
    The logic here wasn't completely correct. A BLS account in FEVM would
    look like an account. It'll return true for precompiles. An inexistent
    address (which could later become an account) will return false.
    
    Not sure if these behaviours were intended, since the method is not
    documented. But given that it's also not used, it's better to drop it so
    it doesn't get used in this form.

--- test/unit/AccountHelper.t.sol
+++ test/unit/AccountHelper.t.sol
@@ -6,53 +6,8 @@ import "forge-std/Test.sol";
 import "fevmate/utils/FilAddress.sol";
 import "../../src/lib/AccountHelper.sol";
 
-contract DummyContract {
-    using AccountHelper for address;
-
-    bool public isAccount;
-
-    constructor() {
-        isAccount = address(this).isAccount();
-    }
-}
-
 contract AccountHelperTest is Test {
     using AccountHelper for address;
-
-    address constant ETH_ADDRESS = address(100);
-    address constant BLS_ADDREESS = 0xfF000000000000000000000000000000bEefbEEf;
-
-    function test_IsAccount_Fails_NonExistingAccount() public view {
-        require(ETH_ADDRESS.isAccount() == false);
-    }
-
-    function test_IsAccount_Fails_BlsAccount() public view {
-        require(BLS_ADDREESS.isAccount() == false);
-    }
-
-    function test_IsAccount_Works_ContractConstructor() public {
-        DummyContract dc = new DummyContract();
-
-        require(dc.isAccount() == true);
-    }
-
-    function test_IsAccount_Fails_ContractAccount() public {
-        DummyContract dc = new DummyContract();
-
-        require(address(dc).isAccount() == false);
-    }
-
-    function test_IsAccount_Works_EthAccount() public {
-        activateAccount(ETH_ADDRESS);
-
-        require(ETH_ADDRESS.isAccount() == true);
-    }
-
-    function test_IsAccount_Works_BlsAccount() public {
-        activateAccount(BLS_ADDREESS);
-
-        require(BLS_ADDREESS.isAccount() == true);
-    }
 
     function test_IsSystemActor_True() public pure {
         require(FilAddress.SYSTEM_ACTOR.isSystemActor() == true);
