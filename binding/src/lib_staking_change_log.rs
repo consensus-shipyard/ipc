aa0d96d06183fd38c1fe5e090a4062b7b48f85c9
auto-commit <auto-commit@users.noreply.github.com>
2023-12-19 21:54:38 +0000

    GEN: commit rust binding

--- binding/src/lib_staking_change_log.rs
+++ binding/src/lib_staking_change_log.rs
@@ -62,13 +62,13 @@ pub mod lib_staking_change_log {
         ::ethers::core::abi::Abi,
     > = ::ethers::contract::Lazy::new(__abi);
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`\x80\x80`@R4`\x17W`:\x90\x81`\x1D\x8290\x81PP\xF3[`\0\x80\xFD\xFE`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xCC\xCDh\x0C3\xBFl\x15\x0E\x9E6\xFB\xF9\xA9\x91\xC5!\x8C\x1F\xA4=\xF7\x98\xBBC\xD4\xF6\xFB\x89\xA0\n\xB9dsolcC\0\x08\x13\x003";
+    const __BYTECODE: &[u8] = b"`\x80\x80`@R4`\x17W`:\x90\x81`\x1D\x8290\x81PP\xF3[`\0\x80\xFD\xFE`\0\x80\xFD\xFE\xA2dipfsX\"\x12 ubV?0k\x95\xAD\x94\xBF4\x07\xAC}\xDA\x97jg\xDE\x06\xD6\x0Ce\xCB\xCC7\xC4X\xF9\rq\x8DdsolcC\0\x08\x13\x003";
     /// The bytecode of the contract.
     pub static LIBSTAKINGCHANGELOG_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __BYTECODE,
     );
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xCC\xCDh\x0C3\xBFl\x15\x0E\x9E6\xFB\xF9\xA9\x91\xC5!\x8C\x1F\xA4=\xF7\x98\xBBC\xD4\xF6\xFB\x89\xA0\n\xB9dsolcC\0\x08\x13\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"`\0\x80\xFD\xFE\xA2dipfsX\"\x12 ubV?0k\x95\xAD\x94\xBF4\x07\xAC}\xDA\x97jg\xDE\x06\xD6\x0Ce\xCB\xCC7\xC4X\xF9\rq\x8DdsolcC\0\x08\x13\x003";
     /// The deployed bytecode of the contract.
     pub static LIBSTAKINGCHANGELOG_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __DEPLOYED_BYTECODE,
