25d319c6bc5efdc0ceeeb7d6ced269eb64e9fe9e
Denis Kolegov <dnkolegov@gmail.com>
2024-01-12 21:13:31 +0100

    Fix minor issues

--- contracts/src/lib/SupplySourceHelper.sol
+++ contracts/src/lib/SupplySourceHelper.sol
@@ -88,7 +88,6 @@ library SupplySourceHelper {
         } else if (supplySource.kind == SupplyKind.ERC20) {
             ret = IERC20(supplySource.tokenAddress).balanceOf(address(this));
         }
-        return ret;
     }
 
     function native() internal pure returns (SupplySource memory) {
