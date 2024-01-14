bc3512fc7c4b0dfcdaac89f297f99cafae68f097
Denis Kolegov <dnkolegov@gmail.com>
2024-01-14 17:17:53 +0100

    Fix issues 37-38

--- contracts/src/lib/LibGatewayActorStorage.sol
+++ contracts/src/lib/LibGatewayActorStorage.sol
@@ -9,71 +9,76 @@ import {Membership} from "../structs/Subnet.sol";
 import {AccountHelper} from "../lib/AccountHelper.sol";
 import {FilAddress} from "fevmate/utils/FilAddress.sol";
 
-struct GatewayActorStorage {
-    /// @notice List of subnets
-    /// SubnetID => Subnet
-    mapping(bytes32 => Subnet) subnets;
-    /// @notice The parent finalities. Key is the block number, value is the finality struct.
-    mapping(uint256 => ParentFinality) finalitiesMap;
-    /// @notice The latest parent height committed.
-    uint256 latestParentHeight;
-    /// @notice Postbox keeps track of all the cross-net messages triggered by
-    /// an actor that need to be propagated further through the hierarchy.
-    /// cross-net message id => CrossMsg
-    mapping(bytes32 => CrossMsg) postbox;
-    /// @notice The current membership of the child subnet
-    Membership currentMembership;
-    /// @notice The last membership received from the parent and adopted
-    Membership lastMembership;
-    /// @notice A mapping of block numbers to bottom-up checkpoints
-    // slither-disable-next-line uninitialized-state
-    mapping(uint256 => BottomUpCheckpoint) bottomUpCheckpoints;
-    /// @notice A mapping of block numbers to bottom-up cross-messages
-    // slither-disable-next-line uninitialized-state
-    mapping(uint256 => BottomUpMsgBatch) bottomUpMsgBatches;
-    /// @notice Quorum information for checkpoints
-    QuorumMap checkpointQuorumMap;
-    /// @notice Quorum information for bottom-up msg batches
-    QuorumMap bottomUpMsgBatchQuorumMap;
-    /// @notice Keys of the registered subnets. Useful to iterate through them
-    bytes32[] subnetKeys;
-    /// @notice path to the current network
-    SubnetID networkName;
-    /// @notice minimum fee amount charged per cross message
-    uint256 minCrossMsgFee;
-    /// @notice majority percentage value (must be greater than or equal to 51)
-    uint8 majorityPercentage;
-    /// @notice nonce for bottom-up messages
-    uint64 bottomUpNonce;
-    /// @notice AppliedNonces keep track of the next nonce of the message to be applied.
-    /// This prevents potential replay attacks.
-    uint64 appliedTopDownNonce;
-    /// @notice Number of active subnets spawned from this one
-    uint64 totalSubnets;
-    /// @notice bottom-up period in number of epochs for the subnet
-    uint256 bottomUpCheckPeriod;
-    /// @notice bottom-up message batch period in number of epochs for the subnet
-    uint256 bottomUpMsgBatchPeriod;
-    /// @notice Maximum number of messages per batch
-    uint64 maxMsgsPerBottomUpBatch;
-    /// Tracking validator changes from parent in child subnet
-    ParentValidatorsTracker validatorsTracker;
-    //
-    // == Feature flags ==
-    /// @notice Determines the maximum depth that this instance of the gateway
-    /// will enforce. Bear in mind that the deployment is decentralized,
-    /// and a subnet could choose not to change this code and not enforce
-    /// this as a maximum depth in its own subnet.
-    uint8 maxTreeDepth;
-    /// @notice Determines if general purpose cros-net messages are supported
-    bool generalPurposeCrossMsg;
-    /// @notice Determines if multi-level cross-net messages are enbaled.
-    bool multiLevelCrossMsg;
-    /// @notice Determines if relayers should be rewarded for checkpoint submissions
-    bool checkpointRelayerRewards;
-    /// @notice Determines if relayers should be rewarded for cross-net message execution
-    bool crossMsgRelayerRewards;
-}
+    struct GatewayActorStorage {
+        /// @notice The latest parent height committed.
+        uint256 latestParentHeight;
+        /// @notice minimum fee amount charged per cross message
+        uint256 minCrossMsgFee;
+        /// @notice bottom-up period in number of epochs for the subnet
+        uint256 bottomUpCheckPeriod;
+        /// @notice bottom-up message batch period in number of epochs for the subnet
+        uint256 bottomUpMsgBatchPeriod;
+        /// @notice nonce for bottom-up messages
+        uint64 bottomUpNonce;
+        /// @notice AppliedNonces keep track of the next nonce of the message to be applied.
+        /// This prevents potential replay attacks.
+        uint64 appliedTopDownNonce;
+        /// @notice Number of active subnets spawned from this one
+        uint64 totalSubnets;
+        /// @notice Maximum number of messages per batch
+        uint64 maxMsgsPerBottomUpBatch;
+        /// @notice majority percentage value (must be greater than or equal to 51)
+        uint8 majorityPercentage;
+        //
+        // == Feature flags ==
+        //
+        /// @notice Determines the maximum depth that this instance of the gateway
+        /// will enforce. Bear in mind that the deployment is decentralized,
+        /// and a subnet could choose not to change this code and not enforce
+        /// this as a maximum depth in its own subnet.
+        uint8 maxTreeDepth;
+        /// @notice Determines if general purpose cros-net messages are supported
+        bool generalPurposeCrossMsg;
+        /// @notice Determines if multi-level cross-net messages are enbaled.
+        bool multiLevelCrossMsg;
+        /// @notice Determines if relayers should be rewarded for checkpoint submissions
+        bool checkpointRelayerRewards;
+        /// @notice Determines if relayers should be rewarded for cross-net message execution
+        bool crossMsgRelayerRewards;
+        // == Structs ==
+        /// @notice The current membership of the child subnet
+        Membership currentMembership;
+        /// @notice The last membership received from the parent and adopted
+        Membership lastMembership;
+        /// @notice Quorum information for checkpoints
+        QuorumMap checkpointQuorumMap;
+        /// @notice Quorum information for bottom-up msg batches
+        QuorumMap bottomUpMsgBatchQuorumMap;
+        /// @notice path to the current network
+        SubnetID networkName;
+        /// Tracking validator changes from parent in child subnet
+        ParentValidatorsTracker validatorsTracker;
+        //
+        // == Dynamic types ==
+        //
+        /// @notice List of subnets
+        /// SubnetID => Subnet
+        mapping(bytes32 => Subnet) subnets;
+        /// @notice The parent finalities. Key is the block number, value is the finality struct.
+        mapping(uint256 => ParentFinality) finalitiesMap;
+        /// @notice Postbox keeps track of all the cross-net messages triggered by
+        /// an actor that need to be propagated further through the hierarchy.
+        /// cross-net message id => CrossMsg
+        mapping(bytes32 => CrossMsg) postbox;
+        /// @notice A mapping of block numbers to bottom-up checkpoints
+        // slither-disable-next-line uninitialized-state
+        mapping(uint256 => BottomUpCheckpoint) bottomUpCheckpoints;
+        /// @notice A mapping of block numbers to bottom-up cross-messages
+        // slither-disable-next-line uninitialized-state
+        mapping(uint256 => BottomUpMsgBatch) bottomUpMsgBatches;
+        /// @notice Keys of the registered subnets. Useful to iterate through them
+        bytes32[] subnetKeys;
+    }
 
 library LibGatewayActorStorage {
     function appStorage() internal pure returns (GatewayActorStorage storage ds) {
