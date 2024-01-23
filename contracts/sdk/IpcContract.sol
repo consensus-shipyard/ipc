4db9511e89dea5662a80efa607a6746c5aa905a8
raulk <raul.kripalani@gmail.com>
2024-01-23 01:20:05 +0000

    adjust the SDK.

--- contracts/sdk/IpcContract.sol
+++ contracts/sdk/IpcContract.sol
@@ -1,151 +1,112 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity 0.8.19;
 
-import {IpcEnvelope, ReceiptMsg, IpcMsg, IpcMsgKind} from "../src/structs/CrossNet.sol";
-import {EMPTY_BYTES} from "../src/constants/Constants.sol";
-import {IGateway} from "../src/interfaces/IGateway.sol";
+import { IpcEnvelope, ResultMsg, CallMsg, IpcMsgKind } from "../src/structs/CrossNet.sol";
+import { IPCAddress } from "../src/structs/Subnet.sol";
+import { EMPTY_BYTES } from "../src/constants/Constants.sol";
+import { IGateway } from "../src/interfaces/IGateway.sol";
+import { ReentrancyGuard } from "openzeppelin-contracts/security/ReentrancyGuard.sol";
+import { Ownable } from "openzeppelin-contracts/access/Ownable.sol";
 
-// Interface that needs to be implemented by IPC-enabled contracts.
-// This is really convenient to call it from other contracts.
-interface IfaceIpcContract {
-    /// @notice Entrypoint for cross-net messages in IPC-enabled contracts.
-    // solhint-disable-next-line func-name-mixedcase
-    function IpcEntrypoint(IpcEnvelope calldata crossMsg) external payable returns (bytes memory ret);
-}
-
-abstract contract IpcContract {
-    // list of messages in-flight for which the contract
-    // hasn't received an ACK.
-    mapping(bytes32 => IpcEnvelope) public inFlightMsgs;
-    // auxiliary mapping to track the height at which certain
-    // in-flight messages were received.
-    mapping(uint256 => bytes32[]) public inFlightMsgHeights;
-    // last height where there messages were pruned
-    uint256 private lastGcBlock;
-
-    // The adderss of the gateway in the subnet.
-    address public gatewayAddr;
-    /// Number of blocks that the contract will wait for a receipt
-    /// before garbage collecting the in-flight message.
-    uint256 public timeoutBlocks;
-
-    constructor(address gatewayAddr_, uint256 timeoutBlocks_) {
-        gatewayAddr = gatewayAddr_;
-        timeoutBlocks = timeoutBlocks_;
-    }
-
+// Interface that needs to be implemented by IPC-aware contracts.
+//
+// TODO: extract to a shared module, so that both the IPC contracts and the SDK can depend on this.
+//  (IPC contracts reaching into the SDK is wrong).
+interface IpcHandler {
     error CallerIsNotGateway();
-    error IpcMsgCallFailed();
     error UnsupportedMsgKind();
     error UnrecognizedReceipt();
+
+    /// @notice Entrypoint for handling xnet messages in IPC-aware contracts.
+    function handleIpcMessage(IpcEnvelope calldata envelope) external payable returns (bytes memory ret);
+}
+
+abstract contract IpcExchange is IpcHandler, Ownable {
+    // The address of the gateway in the network.
+    address public immutable gatewayAddr;
+
+    // List of messages in-flight for which the contract hasn't received a receipt yet.
+    mapping(bytes32 => IpcEnvelope) public inflightMsgs;
+
+    constructor(address gatewayAddr_) {
+        gatewayAddr = gatewayAddr_;
+    }
 
     /// @notice Entrypoint for IPC-enabled contracts. This function is always called by
     /// the gateway when a `Call` or `Receipt` cross-net messages is targeted to
     /// a specific address in the subnet.
-    function IpcEntrypoint(IpcEnvelope calldata crossMsg) external returns (bytes memory) {
-        // trigger the garbage collection of in-flight messages
-        // in case any timeout has triggered already.
-        // timeout == 0 means that no in-flight expiraton is set for receipts.
-        if (timeoutBlocks != 0) {
-            inFlightGC();
-        }
-
-        // only the gateway address is allowed to deliver cross-net messages.
-        if (msg.sender != gatewayAddr) {
-            revert CallerIsNotGateway();
-        }
-
+    function handleIpcMessage(IpcEnvelope calldata envelope) external payable onlyGateway returns (bytes memory) {
         // internal dispatch of the cross-net message to the right method.
-        if (crossMsg.kind == IpcMsgKind.Call) {
-            IpcMsg memory callMsg = abi.decode(crossMsg.message, (IpcMsg));
+        if (envelope.kind == IpcMsgKind.Call) {
+            CallMsg memory call = abi.decode(envelope.message, (CallMsg));
+            return _handleIpcCall(envelope, call);
+        } else if (envelope.kind == IpcMsgKind.Receipt) {
+            ResultMsg memory result = abi.decode(envelope.message, (ResultMsg));
 
-            (bool success, bytes memory ret) = address(this).delegatecall(
-                abi.encodeWithSelector(callMsg.method, callMsg.params)
-            );
-            if (!success) {
-                revert IpcMsgCallFailed();
-            }
-
-            return ret;
-        } else if (crossMsg.kind == IpcMsgKind.Receipt) {
-            ReceiptMsg memory ackMsg = abi.decode(crossMsg.message, (ReceiptMsg));
-
-            // get the original message and chekc if the receipt was generated
-            // by the target contract, and that we actually sent that cross-net message.
-            // If this is not the case, revert, as someone may be trying to do something
-            // sketchy.
-            IpcEnvelope memory originalMsg = inFlightMsgs[ackMsg.id];
+            // Recover the original message.
+            // If we were not tracking it, or if some details don't match, refuse to handle the receipt.
+            IpcEnvelope storage orig = inflightMsgs[result.id];
             if (
-                originalMsg.message.length == 0 ||
-                keccak256(abi.encode(crossMsg.from)) != keccak256(abi.encode(originalMsg.to))
+                orig.message.length == 0 ||
+                keccak256(abi.encode(envelope.from)) != keccak256(abi.encode(orig.to))
             ) {
-                revert UnrecognizedReceipt();
+                revert IpcHandler.UnrecognizedReceipt();
             }
-            // process ACK message and remove from in-flight if it succeeds.
-            if (AckEntrypoint(ackMsg)) {
-                delete inFlightMsgs[ackMsg.id];
-            }
-        } else {
-            revert UnsupportedMsgKind();
+
+            /// Note: if the result handler reverts, we will
+            _handleIpcResult(orig, envelope, result);
+            delete inflightMsgs[result.id];
+            return EMPTY_BYTES;
         }
-
-        return EMPTY_BYTES;
+        revert UnsupportedMsgKind();
     }
 
-    /// @notice Function that needs to be called internally by the contract to send a
-    /// cross-net message.
-    /// TODO: Consider a more Solidity-friendly interface instead of having to pass
-    /// the full envelope?
-    function XnetCall(IpcEnvelope memory crossMsg) internal virtual {
-        // Queue the cross-net message for propagation.
-        IGateway(gatewayAddr).sendContractXnetMessage(crossMsg);
+    /// @notice Function to be overridden by the child contract to handle incoming IPC calls.
+    ///
+    /// NOTE: It's fine for this method to revert. If that happens, IPC will carry the error to the caller.
+    function _handleIpcCall(IpcEnvelope memory envelope, CallMsg memory callMsg) internal virtual returns (bytes memory);
 
-        // Add the message to the list of in-flights
-        bytes32 msgID = keccak256(abi.encode(crossMsg));
-        inFlightMsgs[msgID] = crossMsg;
-        inFlightMsgHeights[block.number].push(msgID);
+    /// @notice Function to be overridden by the child contract to handle results from previously performed IPC calls.
+    ///
+    /// NOTE: This must not revert as doing so will leave the correlation map in an inconsistent state.
+    /// (IPC will consider the result delivery attempted, and will not repeat it again).
+    function _handleIpcResult(IpcEnvelope storage original, IpcEnvelope memory result, ResultMsg memory resultMsg) internal virtual;
+
+    /// @notice Method the implementation of this contract can invoke to perform an IPC call.
+    function performIpcCall(IPCAddress calldata to, CallMsg calldata callMsg, uint256 value) internal virtual {
+        // Queue the cross-net message for dispatch.
+        IpcEnvelope memory envelope = IGateway(gatewayAddr).sendContractXnetMessage{value: value}(IpcEnvelope({
+            kind: IpcMsgKind.Call,
+            from: to, // TODO: will anyway be replaced by sendContractXnetMessage.
+            to: to,
+            nonce: 0, // TODO: will be replaced.
+            value: value,
+            message: abi.encode(callMsg)
+        }));
+        // Add the message to the list of inflights
+        bytes32 id = keccak256(abi.encode(envelope));
+        inflightMsgs[id] = envelope;
     }
 
-    /// @notice Garbage collect in-flight messages for which the
-    /// timeout has been reached.
-    // TODO: We could probably abstract this logic into a helper
-    // with the contract variables for readibility and convenience.
-    function inFlightGC() internal {
-        // if the timeout hasn't been reached, nothing to do
-        if (block.number > lastGcBlock + timeoutBlocks) {
-            // garbage collect from lastGcBlock to the latest height that is
-            // garbage collectable.
-            // e.g. lastGcBlock = 100; timeoutBlocks = 10; block.number = 120
-            // Garbage collect: [100, ..., 110]
-            for (uint256 i = lastGcBlock; i <= block.number - timeoutBlocks; ) {
-                bytes32[] memory msgIds = inFlightMsgHeights[i];
-                // remove the specific messages and the heigth trakcer
-                for (uint256 j = 0; i < msgIds.length; ) {
-                    delete inFlightMsgs[msgIds[j]];
-                    unchecked {
-                        ++j;
-                    }
-                }
-                delete inFlightMsgHeights[i];
-                unchecked {
-                    ++i;
-                }
+    function dropMessages(bytes32[] calldata ids) public onlyOwner {
+        uint256 length = ids.length;
+        for (uint256 i; i < length; ) {
+            delete inflightMsgs[ids[i]];
+            unchecked {
+                ++i;
             }
-            // track last garbage collection height
-            lastGcBlock = block.number;
         }
     }
 
-    /// @notice Implements the logic of the contract when it receives a receipt.
-    /// It should handle success and failures from calls to cross-net messages.
-    /// When a failure receipt is received, the side-effects triggered through
-    /// the cross-net call should be compensated.
-    /* solhint-disable-next-line unused-vars */
-    function AckEntrypoint(ReceiptMsg memory receipt) internal virtual returns (bool) {
-        // This function is use-case specific and should be overwritten
-        // explicitly by contract developers.
-        // It should explicitly handle the compensation from a NAck
-        // (ACKs may not need an explicit handler in many cases).
-        return true;
+    function _onlyGateway() private view {
+        // only the gateway address is allowed to deliver xnet messages.
+        if (msg.sender != gatewayAddr) {
+            revert IpcHandler.CallerIsNotGateway();
+        }
+    }
+
+    modifier onlyGateway() {
+        _onlyGateway();
+        _;
     }
 }
