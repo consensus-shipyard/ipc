ea6cc548f91c02ef00bc04e3755769d31114b87d
mike seiler <snissn@gmail.com>
2024-02-07 20:03:31 -1000

    revert unwanted whitespace change

--- contracts/src/lib/SupplySourceHelper.sol
+++ contracts/src/lib/SupplySourceHelper.sol
@@ -147,6 +147,7 @@ library SupplySourceHelper {
      */
     function sendValue(address payable recipient, uint256 amount) internal returns (bool) {
         require(address(this).balance >= amount, "insufficient balance");
+
         (bool success, ) = recipient.call{value: amount}("");
         return success;
     }
