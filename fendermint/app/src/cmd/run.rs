00d1dcb0f6a5f7ce1daf95ef2a81ea5aaded5feb
Fridrik Asmundsson <fridrik01@gmail.com>
2024-02-10 17:48:12 +0000

    Fetch clients.url form repository secret

--- fendermint/app/src/cmd/run.rs
+++ fendermint/app/src/cmd/run.rs
@@ -56,8 +56,6 @@ namespaces! {
 ///
 /// This method acts as our composition root.
 async fn run(settings: Settings) -> anyhow::Result<()> {
-    println!("Starting fendermint, opts: {:?}", settings);
-
     let tendermint_rpc_url = settings.tendermint_rpc_url()?;
     tracing::info!("Connecting to Tendermint at {tendermint_rpc_url}");
 
