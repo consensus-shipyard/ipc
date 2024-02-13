38c5f1b3b40f9972d2c5411512f48bbd4133983e
Akosh Farkash <aakoshh@gmail.com>
2024-02-13 16:45:24 +0000

    ENG-578: Cleanup of types

--- fendermint/testing/materializer/src/docker.rs
+++ fendermint/testing/materializer/src/docker.rs
@@ -2,12 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use async_trait::async_trait;
-use std::path::{Path, PathBuf};
+use ethers::{
+    core::rand::{rngs::StdRng, SeedableRng},
+    types::H160,
+};
+use fendermint_vm_genesis::Collateral;
+use std::{
+    collections::BTreeMap,
+    path::{Path, PathBuf},
+};
+use tendermint_rpc::Url;
 
 use crate::{
-    materializer::Materializer,
+    manifest::Balance,
+    materializer::{Materializer, NodeConfig, SubmitConfig, SubnetConfig},
     materials::{DefaultAccount, DefaultDeployment, DefaultGenesis, DefaultSubnet, Materials},
-    NodeName, TestnetName,
+    NodeName, RelayerName, ResourceHash, ResourceName, SubnetName, TestnetName,
 };
 
 pub struct DockerMaterials;
@@ -21,16 +31,6 @@ impl Materials for DockerMaterials {
     type Network = DockerNetwork;
     type Node = DockerNode;
     type Relayer = DockerRelayer;
-}
-
-pub struct DockerMaterializer {
-    dir: PathBuf,
-}
-
-impl DockerMaterializer {
-    pub fn new(dir: &Path) -> Self {
-        Self { dir: dir.into() }
-    }
 }
 
 pub struct DockerNetwork {
@@ -58,6 +58,28 @@ pub struct DockerRelayer {
     relayer_container_name: String,
 }
 
+pub struct DockerMaterializer {
+    dir: PathBuf,
+    rng: StdRng,
+}
+
+impl DockerMaterializer {
+    /// Create a materializer with a directory where all the
+    /// testnets can live next to each other.
+    pub fn new(dir: &Path, seed: u64) -> Self {
+        Self {
+            dir: dir.into(),
+            rng: StdRng::seed_from_u64(seed),
+        }
+    }
+
+    /// Path to a directory based on a resource name.
+    fn path<T: AsRef<ResourceName>>(&self, name: T) -> PathBuf {
+        let name: &ResourceName = name.as_ref();
+        self.dir.join(&name.0)
+    }
+}
+
 #[async_trait]
 impl Materializer<DockerMaterials> for DockerMaterializer {
     async fn create_network(
@@ -67,17 +89,18 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
         todo!()
     }
 
+    /// Create a new key-value pair, or return an existing one.
     fn create_account(
         &mut self,
         account_name: &crate::AccountName,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Account> {
-        todo!()
+    ) -> anyhow::Result<DefaultAccount> {
+        DefaultAccount::get_or_create(&mut self.rng, &self.dir, account_name)
     }
 
     async fn fund_from_faucet<'s, 'a>(
         &'s mut self,
-        account: &'a <DockerMaterials as Materials>::Account,
-        reference: Option<crate::ResourceHash>,
+        account: &'a DefaultAccount,
+        reference: Option<ResourceHash>,
     ) -> anyhow::Result<()>
     where
         's: 'a,
@@ -87,10 +110,10 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     async fn new_deployment<'s, 'a>(
         &'s mut self,
-        subnet_name: &crate::SubnetName,
-        deployer: &'a <DockerMaterials as Materials>::Account,
-        urls: Vec<tendermint_rpc::Url>,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Deployment>
+        subnet_name: &SubnetName,
+        deployer: &'a DefaultAccount,
+        urls: Vec<Url>,
+    ) -> anyhow::Result<DefaultDeployment>
     where
         's: 'a,
     {
@@ -99,40 +122,34 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     fn existing_deployment(
         &mut self,
-        subnet_name: &crate::SubnetName,
-        gateway: ethers::types::H160,
-        registry: ethers::types::H160,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Deployment> {
+        subnet_name: &SubnetName,
+        gateway: H160,
+        registry: H160,
+    ) -> anyhow::Result<DefaultDeployment> {
         todo!()
     }
 
     fn default_deployment(
         &mut self,
-        subnet_name: &crate::SubnetName,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Deployment> {
+        subnet_name: &SubnetName,
+    ) -> anyhow::Result<DefaultDeployment> {
         todo!()
     }
 
     fn create_root_genesis<'a>(
         &mut self,
-        subnet_name: &crate::SubnetName,
-        validators: std::collections::BTreeMap<
-            &'a <DockerMaterials as Materials>::Account,
-            fendermint_vm_genesis::Collateral,
-        >,
-        balances: std::collections::BTreeMap<
-            &'a <DockerMaterials as Materials>::Account,
-            crate::manifest::Balance,
-        >,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Genesis> {
+        subnet_name: &SubnetName,
+        validators: BTreeMap<&'a DefaultAccount, Collateral>,
+        balances: BTreeMap<&'a DefaultAccount, Balance>,
+    ) -> anyhow::Result<DefaultGenesis> {
         todo!()
     }
 
     async fn create_node<'s, 'a>(
         &'s mut self,
         node_name: &NodeName,
-        node_config: crate::materializer::NodeConfig<'a, DockerMaterials>,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Node>
+        node_config: NodeConfig<'a, DockerMaterials>,
+    ) -> anyhow::Result<DockerNode>
     where
         's: 'a,
     {
@@ -141,8 +158,8 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     async fn start_node<'s, 'a>(
         &'s mut self,
-        node: &'a <DockerMaterials as Materials>::Node,
-        seed_nodes: &'a [&'a <DockerMaterials as Materials>::Node],
+        node: &'a DockerNode,
+        seed_nodes: &'a [&'a DockerNode],
     ) -> anyhow::Result<()>
     where
         's: 'a,
@@ -152,10 +169,10 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     async fn create_subnet<'s, 'a>(
         &'s mut self,
-        parent_submit_config: &crate::materializer::SubmitConfig<'a, DockerMaterials>,
-        subnet_name: &crate::SubnetName,
-        subnet_config: crate::materializer::SubnetConfig<'a, DockerMaterials>,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Subnet>
+        parent_submit_config: &SubmitConfig<'a, DockerMaterials>,
+        subnet_name: &SubnetName,
+        subnet_config: SubnetConfig<'a, DockerMaterials>,
+    ) -> anyhow::Result<DefaultSubnet>
     where
         's: 'a,
     {
@@ -164,11 +181,11 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     async fn fund_subnet<'s, 'a>(
         &'s mut self,
-        parent_submit_config: &crate::materializer::SubmitConfig<'a, DockerMaterials>,
-        account: &'a <DockerMaterials as Materials>::Account,
-        subnet: &'a <DockerMaterials as Materials>::Subnet,
+        parent_submit_config: &SubmitConfig<'a, DockerMaterials>,
+        account: &'a DefaultAccount,
+        subnet: &'a DefaultSubnet,
         amount: fvm_shared::econ::TokenAmount,
-        reference: Option<crate::ResourceHash>,
+        reference: Option<ResourceHash>,
     ) -> anyhow::Result<()>
     where
         's: 'a,
@@ -178,12 +195,12 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     async fn join_subnet<'s, 'a>(
         &'s mut self,
-        parent_submit_config: &crate::materializer::SubmitConfig<'a, DockerMaterials>,
-        account: &'a <DockerMaterials as Materials>::Account,
-        subnet: &'a <DockerMaterials as Materials>::Subnet,
+        parent_submit_config: &SubmitConfig<'a, DockerMaterials>,
+        account: &'a DefaultAccount,
+        subnet: &'a DefaultSubnet,
         collateral: fendermint_vm_genesis::Collateral,
-        balance: crate::manifest::Balance,
-        reference: Option<crate::ResourceHash>,
+        balance: Balance,
+        reference: Option<ResourceHash>,
     ) -> anyhow::Result<()>
     where
         's: 'a,
@@ -193,9 +210,9 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     async fn create_subnet_genesis<'s, 'a>(
         &'s mut self,
-        parent_submit_config: &crate::materializer::SubmitConfig<'a, DockerMaterials>,
-        subnet: &'a <DockerMaterials as Materials>::Subnet,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Genesis>
+        parent_submit_config: &SubmitConfig<'a, DockerMaterials>,
+        subnet: &'a DefaultSubnet,
+    ) -> anyhow::Result<DefaultGenesis>
     where
         's: 'a,
     {
@@ -204,12 +221,12 @@ impl Materializer<DockerMaterials> for DockerMaterializer {
 
     async fn create_relayer<'s, 'a>(
         &'s mut self,
-        parent_submit_config: &crate::materializer::SubmitConfig<'a, DockerMaterials>,
-        relayer_name: &crate::RelayerName,
-        subnet: &'a <DockerMaterials as Materials>::Subnet,
-        submitter: &'a <DockerMaterials as Materials>::Account,
-        follow_node: &'a <DockerMaterials as Materials>::Node,
-    ) -> anyhow::Result<<DockerMaterials as Materials>::Relayer>
+        parent_submit_config: &SubmitConfig<'a, DockerMaterials>,
+        relayer_name: &RelayerName,
+        subnet: &'a DefaultSubnet,
+        submitter: &'a DefaultAccount,
+        follow_node: &'a DockerNode,
+    ) -> anyhow::Result<DockerRelayer>
     where
         's: 'a,
     {
