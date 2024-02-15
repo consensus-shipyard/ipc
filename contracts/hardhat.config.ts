ea098a915146939467bc510efffb197d0abb6f51
raulk <raul.kripalani@gmail.com>
2024-02-15 23:16:51 +0000

    hardhat: bump Solidity compiler.

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -333,7 +333,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: '0.8.19',
+                version: '0.8.20',
                 settings: {
                     viaIR: true,
                     optimizer: {
