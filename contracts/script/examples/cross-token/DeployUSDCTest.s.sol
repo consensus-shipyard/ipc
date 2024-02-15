e9b5845fad020f7f1af17674cd6cd5efeb46f5b0
mike seiler <snissn@gmail.com>
2024-02-15 10:53:53 -0700

    add forge script for deploying usdc test contract

---
+++ contracts/script/examples/cross-token/DeployUSDCTest.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "../../../src/examples/cross-token/USDCTest.sol";
+
+contract DeployUSDCTest is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        USDCTest erc20Token = new USDCTest();
+
+        vm.stopBroadcast();
+    }
+}
