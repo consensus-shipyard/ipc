0855f0fd0b267942e3e2e4b3bf5ff8d5fbe0596d
raulk <raul@protocol.ai>
2024-02-16 17:39:20 +0000

    upgrade Solidity to ^0.8.23; OpenZeppelin to v5.0.1. (#710)

--- contracts/src/enums/ConsensusType.sol
+++ contracts/src/enums/ConsensusType.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity 0.8.19;
+pragma solidity ^0.8.23;
 
 /// @title Subnet Consensus Type enum
 /// @author LimeChain team
--- contracts/src/enums/IPCMsgType.sol
+++ contracts/src/enums/IPCMsgType.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity 0.8.19;
+pragma solidity ^0.8.23;
 
 /// @title IPC Message type enum
 /// @author LimeChain team
