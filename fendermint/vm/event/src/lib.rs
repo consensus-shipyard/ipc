4d3f8901c8be2116cbab9c3416e1aba8bbf16724
cryptoAtwill <108330426+cryptoAtwill@users.noreply.github.com>
2024-02-21 09:59:56 +0800

    Adding initial events from fendermint (#723)

---
+++ fendermint/vm/event/src/lib.rs
@@ -0,0 +1,21 @@
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+#[derive(strum::Display)]
+pub enum EventType {
+    NewParentView,
+    ParentFinalityCommitted,
+    NewBottomUpCheckpoint,
+    /// A new block is produced in fendermint
+    NewBlock,
+}
+
+#[macro_export]
+macro_rules! emit {
+    ($event:expr, $($arg:tt)*) => {
+        tracing::info!(event = tracing::field::display($event), $($arg)+)
+    };
+    ($event:expr) => {
+        tracing::info!(event = tracing::field::display($event))
+    };
+}
