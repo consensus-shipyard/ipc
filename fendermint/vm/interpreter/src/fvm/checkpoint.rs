ffa29015f0f8e2d4b4f7c3e5d624218a94c13444
cryptoAtwill <willes.lau@protocol.ai>
2024-03-07 14:33:19 +0800

    add configuration number

--- fendermint/vm/interpreter/src/fvm/checkpoint.rs
+++ fendermint/vm/interpreter/src/fvm/checkpoint.rs
@@ -122,7 +122,7 @@ where
 
         debug_assert_eq!(next_power_configuration_number, next_configuration_number);
 
-        tracing::info!(curr = ?curr_power_table, next = ?next_power_table, "tables");
+        tracing::info!(next_configuration_number, curr = ?curr_power_table, next = ?next_power_table, "tables");
         power_diff(curr_power_table, next_power_table)
     };
 
