476b472b50be43438c568e592684f41b6ea0551b
Akosh Farkash <aakoshh@gmail.com>
2024-03-20 14:45:52 +0000

    SIG: Allow eth to send to non-eth with regular signature (#773)

--- fendermint/vm/message/src/signed.rs
+++ fendermint/vm/message/src/signed.rs
@@ -7,11 +7,13 @@ use cid::multihash::MultihashDigest;
 use cid::Cid;
 use ethers_core::types as et;
 use ethers_core::types::transaction::eip2718::TypedTransaction;
-use fendermint_crypto::SecretKey;
+use fendermint_crypto::{PublicKey, SecretKey};
+use fendermint_vm_actor_interface::eam::EthAddress;
 use fendermint_vm_actor_interface::{eam, evm};
 use fvm_ipld_encoding::tuple::{Deserialize_tuple, Serialize_tuple};
 use fvm_shared::address::{Address, Payload};
 use fvm_shared::chainid::ChainID;
+use fvm_shared::crypto::signature::ops::recover_secp_public_key;
 use fvm_shared::crypto::signature::{Signature, SignatureType, SECP_SIG_LEN};
 use fvm_shared::message::Message;
 
@@ -24,6 +26,9 @@ enum Signable {
     Ethereum((et::H256, et::H160)),
     /// Bytes to be passed to the FVM Signature for hashing or verification.
     Regular(Vec<u8>),
+    /// Same signature as `Regular` but using an Ethereum account hash as sender.
+    /// This is used if the recipient of the message is not an Ethereum account.
+    RegularFromEth((Vec<u8>, et::H160)),
 }
 
 #[derive(Error, Debug)]
@@ -32,7 +37,7 @@ pub enum SignedMessageError {
     Ipld(#[from] fvm_ipld_encoding::Error),
     #[error("invalid signature: {0}")]
     InvalidSignature(String),
-    #[error("message cannot be converted to ethereum")]
+    #[error("message cannot be converted to ethereum: {0}")]
     Ethereum(#[from] anyhow::Error),
 }
 
@@ -77,6 +82,7 @@ impl SignedMessage {
         let signature = match Self::signable(&message, chain_id)? {
             Signable::Ethereum((hash, _)) => sign_eth(sk, hash),
             Signable::Regular(data) => sign_regular(sk, &data),
+            Signable::RegularFromEth((data, _)) => sign_regular(sk, &data),
         };
         Ok(Self { message, signature })
     }
@@ -104,13 +110,21 @@ impl SignedMessage {
         // so at least for now they are easy to tell apart: any `f410` address is coming from Ethereum API and must have
         // been signed according to the Ethereum scheme, and it could not have been signed by an `f1` address, it doesn't
         // work with regular accounts.
+        //
+        // We detect the case where the recipient is not an ethereum address. If that is the case then use regular signing rules,
+        // which should allow messages from ethereum accounts to go to any other type of account, e.g. custom Wasm actors.
         match maybe_eth_address(&message.from) {
-            Some(addr) => {
+            Some(addr) if is_eth_addr_compat(&message.to) => {
                 let tx: TypedTransaction = from_fvm::to_eth_transaction_request(message, chain_id)
                     .map_err(SignedMessageError::Ethereum)?
                     .into();
 
                 Ok(Signable::Ethereum((tx.sighash(), addr)))
+            }
+            Some(addr) => {
+                let mut data = Self::cid(message)?.to_bytes();
+                data.extend(chain_id_bytes(chain_id).iter());
+                Ok(Signable::RegularFromEth((data, addr)))
             }
             None => {
                 let mut data = Self::cid(message)?.to_bytes();
@@ -149,6 +163,18 @@ impl SignedMessage {
                     .verify(&data, &message.from)
                     .map_err(SignedMessageError::InvalidSignature)
             }
+            Signable::RegularFromEth((data, from)) => {
+                let rec = recover_secp256k1(signature, &data)
+                    .map_err(SignedMessageError::InvalidSignature)?;
+
+                let rec_addr = EthAddress::from(rec);
+
+                if rec_addr.0 == from.0 {
+                    Ok(())
+                } else {
+                    Err(SignedMessageError::InvalidSignature("the Ethereum delegated address did not match the one recovered from the signature".to_string()))
+                }
+            }
         }
     }
 
@@ -157,7 +183,7 @@ impl SignedMessage {
         &self,
         chain_id: &ChainID,
     ) -> Result<Option<DomainHash>, SignedMessageError> {
-        if maybe_eth_address(&self.message.from).is_some() {
+        if is_eth_addr_deleg(&self.message.from) && is_eth_addr_compat(&self.message.to) {
             let tx: TypedTransaction =
                 from_fvm::to_eth_transaction_request(self.message(), chain_id)
                     .map_err(SignedMessageError::Ethereum)?
@@ -245,6 +271,16 @@ fn maybe_eth_address(addr: &Address) -> Option<et::H160> {
     }
 }
 
+/// Check if the address can be converted to an Ethereum one.
+fn is_eth_addr_compat(addr: &Address) -> bool {
+    from_fvm::to_eth_address(addr).is_ok()
+}
+
+/// Check if the address is an Ethereum delegated one.
+fn is_eth_addr_deleg(addr: &Address) -> bool {
+    maybe_eth_address(addr).is_some()
+}
+
 /// Verify that the method ID and the recipient are one of the allowed combination,
 /// which for example is set by [from_eth::to_fvm_message].
 ///
@@ -280,6 +316,36 @@ pub fn sign_secp256k1(sk: &SecretKey, hash: &[u8; 32]) -> Signature {
         sig_type: SignatureType::Secp256k1,
         bytes: signature.to_vec(),
     }
+}
+
+/// Recover the public key from a Secp256k1
+///
+/// Based on how `Signature` does it, but without the final address hashing.
+fn recover_secp256k1(signature: &Signature, data: &[u8]) -> Result<PublicKey, String> {
+    let signature = &signature.bytes;
+
+    if signature.len() != SECP_SIG_LEN {
+        return Err(format!(
+            "Invalid Secp256k1 signature length. Was {}, must be 65",
+            signature.len()
+        ));
+    }
+
+    // blake2b 256 hash
+    let hash = blake2b_simd::Params::new()
+        .hash_length(32)
+        .to_state()
+        .update(data)
+        .finalize();
+
+    let mut sig = [0u8; SECP_SIG_LEN];
+    sig[..].copy_from_slice(signature);
+
+    let rec_key =
+        recover_secp_public_key(hash.as_bytes().try_into().expect("fixed array size"), &sig)
+            .map_err(|e| e.to_string())?;
+
+    Ok(rec_key)
 }
 
 /// Signed message with an invalid random signature.
@@ -384,5 +450,20 @@ mod tests {
             return Err("signature verification should have failed".to_string());
         }
         Ok(())
+    }
+
+    /// Check that we can send from an ethereum account to a non-ethereum one and sign it.
+    #[quickcheck]
+    fn eth_to_non_eth_sign_and_verify(msg: EthMessage, chain_id: u64, from: KeyPair, to: KeyPair) {
+        let chain_id = ChainID::from(chain_id);
+        let mut msg = msg.0;
+
+        msg.from = Address::from(EthAddress::from(from.pk));
+        msg.to = Address::new_secp256k1(&to.pk.serialize()).expect("f1 address");
+
+        let signed =
+            SignedMessage::new_secp256k1(msg, &from.sk, &chain_id).expect("message can be signed");
+
+        signed.verify(&chain_id).expect("signature should be valid")
     }
 }
