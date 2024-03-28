42096240eedaeeefa08a2fb35447c61e9d3b855a
hfccr <hfccr@outlook.com>
2024-03-28 19:44:33 +0530

    Add interface import

--- extras/linked-token/src/StrategyBase.sol
+++ extras/linked-token/src/StrategyBase.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.23;
 import {StrategyManager} from "./StrategyManager.sol";
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {IStrategy} from "./IStrategy.sol";
 
 contract StrategyBase is IStrategy {
     using SafeERC20 for IERC20;
