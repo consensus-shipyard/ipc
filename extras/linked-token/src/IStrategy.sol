94f142cbfb496902caa59d64897ed56dae986cb6
hfccr <hfccr@outlook.com>
2024-03-28 19:42:37 +0530

    Fix import

--- extras/linked-token/src/IStrategy.sol
+++ extras/linked-token/src/IStrategy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {IERC20} from "@openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 interface IStrategy {
     function deposit(IERC20 token, uint256 amount) external returns (uint256);
