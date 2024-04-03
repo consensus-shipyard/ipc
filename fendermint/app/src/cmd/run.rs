587780272e7ff5d4b87d8e27971b1fde54a8ddad
cryptoAtwill <willes.lau@protocol.ai>
2024-04-03 11:09:49 +0800

    expose max cache blocks

--- fendermint/app/src/cmd/run.rs
+++ fendermint/app/src/cmd/run.rs
@@ -219,14 +219,20 @@ async fn run(settings: Settings) -> anyhow::Result<()> {
     let (parent_finality_provider, ipc_tuple) = if topdown_enabled {
         info!("topdown finality enabled");
         let topdown_config = settings.ipc.topdown_config()?;
-        let config = fendermint_vm_topdown::Config::new(
+        let mut config = fendermint_vm_topdown::Config::new(
             topdown_config.chain_head_delay,
             topdown_config.polling_interval,
             topdown_config.exponential_back_off,
             topdown_config.exponential_retry_limit,
         )
-        .with_proposal_delay(topdown_config.proposal_delay)
-        .with_max_proposal_range(topdown_config.max_proposal_range);
+            .with_proposal_delay(topdown_config.proposal_delay)
+            .with_max_proposal_range(topdown_config.max_proposal_range);
+
+        if let Some(v) = topdown_config.max_cache_blocks {
+            info!(value = v, "setting max cache blocks");
+            config = config.with_max_cache_blocks(v);
+        }
+
         let ipc_provider = Arc::new(make_ipc_provider_proxy(&settings)?);
         let finality_provider =
             CachedFinalityProvider::uninitialized(config.clone(), ipc_provider.clone()).await?;
