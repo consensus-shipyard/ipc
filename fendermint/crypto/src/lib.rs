9b0c7de8ee6349cfca69156647219d07afdc4fc3
avichalp <hi@avichalp.me>
2024-04-10 17:10:08 +0800

    Remove unused function
    Signed-off-by: avichalp <hi@avichalp.me>

--- fendermint/crypto/src/lib.rs
+++ fendermint/crypto/src/lib.rs
@@ -4,8 +4,6 @@
 use base64::engine::GeneralPurpose;
 use base64::engine::{DecodePaddingMode, GeneralPurposeConfig};
 use base64::{alphabet, Engine};
-use ethers::{types::Address, utils::keccak256};
-use libsecp256k1::{recover, Message};
 use rand::Rng;
 use zeroize::{Zeroize, ZeroizeOnDrop, Zeroizing};
 
@@ -98,32 +96,4 @@ impl From<&SecretKey> for PublicKey {
     fn from(value: &SecretKey) -> Self {
         value.public_key()
     }
-}
-
-/// Recover a sender, given message and the signature.
-/// and returns the address of a public key.
-///
-/// The public address is defined as the low 20 bytes of the keccak hash of
-/// the public key. Note that the public key returned from the `secp256k1`
-/// crate is 65 bytes long, that is because it is prefixed by `0x04` to
-/// indicate an uncompressed public key; this first byte is ignored when
-/// computing the hash.
-///
-/// Based on https://github.com/tomusdrw/rust-web3/blob/master/src/signing.rs
-pub fn recover_address(
-    message: &[u8; 32],
-    signature: &[u8],
-    recovery_id: u8,
-) -> Result<Address, libsecp256k1::Error> {
-    let message = Message::parse(message);
-    let signature = Signature::parse_standard_slice(signature)?;
-    let recovery_id = RecoveryId::parse(recovery_id)?;
-
-    let public_key = recover(&message, &signature, &recovery_id)?;
-    let public_key_bytes = public_key.serialize();
-    debug_assert_eq!(public_key_bytes[0], 0x04);
-
-    let hash = keccak256(&public_key_bytes[1..]);
-
-    Ok(Address::from_slice(&hash[12..]))
 }
