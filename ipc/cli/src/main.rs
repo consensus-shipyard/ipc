c1834516a0e69311a4cbe0024aa59bdd67f07a16
Mikers <snissn@gmail.com>
2024-05-02 14:13:18 -1000

    bugfix: replace env_logger with tracing_subscriber to enable logging from imported libraries with RUST_LOG env variable

--- ipc/cli/src/main.rs
+++ ipc/cli/src/main.rs
@@ -1,9 +1,16 @@
 // Copyright 2022-2024 Protocol Labs
 // SPDX-License-Identifier: MIT
 
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+
 #[tokio::main]
 async fn main() {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+        tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     if let Err(e) = ipc_cli::cli().await {
         log::error!("main process failed: {e:#}");
