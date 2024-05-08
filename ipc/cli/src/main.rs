151c0e3b2f03a64041afa7c84ab5944a124f7a2c
Mikers <snissn@gmail.com>
2024-05-07 15:58:37 -1000

    Set default to INFO level unless specified by environment

--- ipc/cli/src/main.rs
+++ ipc/cli/src/main.rs
@@ -8,7 +8,7 @@ use tracing_subscriber::{fmt, EnvFilter};
 async fn main() {
     tracing_subscriber::registry()
         .with(fmt::layer())
-        .with(EnvFilter::from_default_env())
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
         .init();
 
     if let Err(e) = ipc_cli::cli().await {
