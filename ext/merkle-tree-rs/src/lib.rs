8a0236f7e8db3ccd2692ff593fe06ec5b3fd67ae
cryptoAtwill <willes.lau@protocol.ai>
2024-05-21 21:21:58 +0800

    review feedbacks

--- ext/merkle-tree-rs/src/lib.rs
+++ ext/merkle-tree-rs/src/lib.rs
@@ -1,5 +1,6 @@
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
 #[allow(clippy::all)]
-
 pub mod core;
 pub mod format;
 pub mod standard;
--- ext/merkle-tree-rs/src/standard.rs
+++ ext/merkle-tree-rs/src/standard.rs
@@ -1,3 +1,5 @@
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
 use anyhow::{anyhow, bail, Result};
 use ethers::{
     abi::{
