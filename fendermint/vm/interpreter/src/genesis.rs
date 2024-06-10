125e704920227c9dd79f6685be195359b8e84fc1
cryptoAtwill <willes.lau@protocol.ai>
2024-06-10 14:10:05 +0800

    remove validators

--- fendermint/vm/interpreter/src/genesis.rs
+++ fendermint/vm/interpreter/src/genesis.rs
@@ -152,17 +152,9 @@ impl GenesisCreator {
 
         tracing::info!(state_root = state_root.to_string(), "state root");
 
-        let metadata = GenesisMetadata::new(state_root, out);
-
-        let streamer = StateTreeStreamer::new(state_root, store);
-        let (metadata_cid, metadata_bytes) = derive_cid(&metadata)?;
-        tracing::info!("generated genesis metadata header cid: {}", metadata_cid);
-
+        let mut streamer = StateTreeStreamer::new(state_root, store);
         // create the target car header with the metadata cid as the only root
-        let car = CarHeader::new(vec![metadata_cid], 1);
-
-        // create the stream to stream all the data into the car file
-        let mut streamer = tokio_stream::iter(vec![(metadata_cid, metadata_bytes)]).merge(streamer);
+        let car = CarHeader::new(vec![state_root], 1);
 
         let write_task = tokio::spawn(async move {
             let mut write = file.compat_write();
