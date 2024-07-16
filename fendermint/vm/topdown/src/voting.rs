66d8dafa64a0338fcbfd530389a36735f5fe03a4
Karel Moravec <moravec.wdd@gmail.com>
2024-07-16 20:38:52 +0200

    feat: fix typo

--- fendermint/vm/topdown/src/voting.rs
+++ fendermint/vm/topdown/src/voting.rs
@@ -8,8 +8,8 @@ use std::hash::Hash;
 use std::{fmt::Debug, time::Duration};
 
 use crate::observe::{
-    HexEncodableBlockHash, ParentFinalityCommitted, ParentFinalityPeerQuorumReached,
-    ParentFinalityPeerVoteReceived, ParentFinalityPeerVoteSent,
+    ParentFinalityCommitted, ParentFinalityPeerQuorumReached, ParentFinalityPeerVoteReceived,
+    ParentFinalityPeerVoteSent,
 };
 use crate::{BlockHash, BlockHeight};
 use ipc_observability::{emit, serde::HexEncodableBlockHash};
