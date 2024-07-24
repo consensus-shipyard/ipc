12079cd2447f495ed0e59742e56fd5e3294f2441
cryptoAtwill <willes.lau@protocol.ai>
2024-07-24 15:01:00 +0800

    use recoverable sig

--- fendermint/vm/topdown/src/voting/payload.rs
+++ fendermint/vm/topdown/src/voting/payload.rs
@@ -4,7 +4,8 @@
 use crate::{BlockHash, BlockHeight, Bytes};
 use anyhow::anyhow;
 use ipc_ipld_resolver::ValidatorKey;
-use libp2p::identity::Keypair;
+use libp2p::identity::PublicKey;
+use secp256k1::ecdsa::{RecoverableSignature, RecoveryId};
 use serde::{Deserialize, Serialize};
 
 pub type Signature = Bytes;
@@ -42,34 +43,98 @@ impl TopdownVote {
 pub struct SignedVote {
     pub(crate) payload: Bytes,
     /// The signature of the signed content using the pubkey
-    signature: Signature,
-    pub(crate) pubkey: ValidatorKey,
+    signature: SignatureInner,
     // TODO: might have to add timestamp against more attacks
 }
 
 impl SignedVote {
     /// Create a new [`SignedVoteRecord`] with the current timestamp
     /// and a signed envelope which can be shared with others.
-    pub fn signed(key: &Keypair, vote: &TopdownVote) -> anyhow::Result<Self> {
+    pub fn signed(
+        key: &libp2p::identity::secp256k1::Keypair,
+        vote: &TopdownVote,
+    ) -> anyhow::Result<Self> {
         let payload = fvm_ipld_encoding::to_vec(vote)?;
-        let signature = key.sign(&payload)?;
-        let pubkey = ValidatorKey::from(key.public());
-        Ok(Self {
-            payload,
-            signature,
-            pubkey,
-        })
+        let signature = SignatureInner::from_secp(key, &payload)?;
+        Ok(Self { payload, signature })
     }
 
     pub fn into_validated_payload(self) -> anyhow::Result<(TopdownVote, Signature, ValidatorKey)> {
-        if !self.pubkey.verify(&self.payload, &self.signature) {
+        let (pubkey, sig) = self.signature.pubkey_with_signature(&self.payload)?;
+        if !pubkey.verify(&self.payload, &sig) {
             Err(anyhow!("invalid validator signature"))
         } else {
             Ok((
                 fvm_ipld_encoding::from_slice(&self.payload)?,
-                self.signature,
-                self.pubkey,
+                sig,
+                ValidatorKey::from(pubkey),
             ))
         }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Hash, PartialOrd, Ord)]
+enum SignatureInner {
+    Secpk1Recoverable { sig: Vec<u8>, rec: u8 },
+}
+
+impl SignatureInner {
+    fn from_secp(
+        keypair: &libp2p::identity::secp256k1::Keypair,
+        payload: &[u8],
+    ) -> anyhow::Result<Self> {
+        let s = secp256k1::SecretKey::from_slice(&keypair.secret().to_bytes())
+            .map_err(|_| anyhow!("cannot parse secret key, should not have happen"))?;
+
+        let secp = secp256k1::Secp256k1::new();
+
+        let (id, sig) = secp
+            .sign_ecdsa_recoverable(
+                &secp256k1::Message::from_hashed_data::<secp256k1::hashes::sha256::Hash>(payload),
+                &s,
+            )
+            .serialize_compact();
+
+        Ok(Self::Secpk1Recoverable {
+            sig: sig.to_vec(),
+            rec: id.to_i32() as u8,
+        })
+    }
+
+    fn pubkey_with_signature(self, payload: &[u8]) -> anyhow::Result<(PublicKey, Signature)> {
+        match self {
+            SignatureInner::Secpk1Recoverable { sig, rec } => {
+                let secp = secp256k1::Secp256k1::new();
+
+                let pubkey = secp.recover_ecdsa(
+                    &secp256k1::Message::from_hashed_data::<secp256k1::hashes::sha256::Hash>(
+                        payload,
+                    ),
+                    &RecoverableSignature::from_compact(&sig, RecoveryId::from_i32(rec as i32)?)?,
+                )?;
+                Ok((
+                    libp2p::identity::secp256k1::PublicKey::try_from_bytes(&pubkey.serialize())?
+                        .into(),
+                    sig,
+                ))
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::voting::payload::SignatureInner;
+    use rand::random;
+
+    #[test]
+    fn test_signing_and_verification() {
+        let payload = (0..1024).map(|_| random::<u8>()).collect::<Vec<_>>();
+        let key = libp2p::identity::secp256k1::Keypair::generate();
+
+        let sig = SignatureInner::from_secp(&key, &payload).unwrap();
+
+        let verified = sig.pubkey_with_signature(&payload).unwrap();
+        assert_eq!(verified.0, (key.public().clone()).into())
     }
 }
