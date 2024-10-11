1d2b1b55f5150a7911e6ae1bba6a456ff34a71eb
Karel Moravec <moravec.wdd@gmail.com>
2024-10-11 22:01:59 +0200

    feat: fmt & lint

--- contracts/contracts/subnet/SubnetActorCheckpointingFacet.sol
+++ contracts/contracts/subnet/SubnetActorCheckpointingFacet.sol
@@ -14,8 +14,6 @@ import {LibSubnetActor} from "../lib/LibSubnetActor.sol";
 import {Pausable} from "../lib/LibPausable.sol";
 import {LibGateway} from "../lib/LibGateway.sol";
 
-import "forge-std/console.sol";
-
 contract SubnetActorCheckpointingFacet is SubnetActorModifiers, ReentrancyGuard, Pausable {
     using EnumerableSet for EnumerableSet.AddressSet;
     using LibValidatorSet for ValidatorSet;
