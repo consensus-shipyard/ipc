5a52eac8e397939ba1d0bb21e0e48bbad64d0427
Karel Moravec <moravec.wdd@gmail.com>
2024-10-16 11:55:25 +0200

    feat: fix comments

--- contracts/contracts/interfaces/IGateway.sol
+++ contracts/contracts/interfaces/IGateway.sol
@@ -62,8 +62,8 @@ interface IGateway {
         IpcEnvelope calldata envelope
     ) external payable returns (IpcEnvelope memory committed);
 
-    /// @notice Propagates all the stored postbox items
-    function propagateAllPostboxMessages() external payable;
+    /// @notice Propagates all the stored messages to destination subnet
+    function propagateAll() external payable;
 
     /// @notice commit the ipc parent finality into storage
     function commitParentFinality(ParentFinality calldata finality) external;
