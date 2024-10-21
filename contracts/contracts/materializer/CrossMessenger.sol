751c6159e98907302aff57569991fd06d99fb3fe
Karel Moravec <moravec.wdd@gmail.com>
2024-10-21 22:35:47 +0200

    feat: add files

---
+++ contracts/contracts/materializer/CrossMessenger.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.23;
+
+import {FvmAddress} from "../structs/FvmAddress.sol";
+import {SubnetID, IPCAddress} from "../structs/Subnet.sol";
+import {IpcEnvelope, IpcMsgKind, CallMsg} from "../structs/CrossNet.sol";
+import {IGateway} from "../interfaces/IGateway.sol";
+import {EMPTY_BYTES, METHOD_SEND} from "../../contracts/constants/Constants.sol";
+
+/// This is a simple helper contract for Materializer to test cross messages.
+contract CrossMessenger {
+    address internal gatewayAddr;
+
+    function setGatewayAddress(address gateway) external {
+        gatewayAddr = gateway;
+    }
+
+    function invokeCrossMessage(
+        IPCAddress memory from,
+        IPCAddress memory to,
+        uint256 value
+    ) external {
+        CallMsg memory message = CallMsg({method: abi.encodePacked(METHOD_SEND), params: EMPTY_BYTES});
+        IpcEnvelope memory envelope = IpcEnvelope({
+            kind: IpcMsgKind.Call,
+            from: from,
+            to: to,
+            value: value,
+            message: abi.encode(message),
+            nonce: 0
+        });
+
+        IGateway(gatewayAddr).sendContractXnetMessage(envelope);
+    }
+}
