b3f6bfd5b7487d7bea0376ef992af99d7fba217d
avichalp <hi@avichalp.me>
2024-10-30 23:01:33 -0700

    blob voting metrics init
    Signed-off-by: avichalp <hi@avichalp.me>

---
+++ fendermint/vm/iroh_resolver/src/observe.rs
@@ -0,0 +1,124 @@
+// Copyright 2024 Textile
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use ipc_observability::{
+    impl_traceable, impl_traceables, lazy_static, register_metrics, serde::HexEncodableBlockHash,
+    Recordable, TraceLevel, Traceable,
+};
+use prometheus::{register_int_counter_vec, register_int_gauge, IntCounterVec, IntGauge, Registry};
+
+register_metrics! {
+    BLOBS_FINALITY_VOTING_SUCCESS: IntCounterVec
+        = register_int_counter_vec!(
+            "blobs_finality_voting_success",
+            "Blobs finality: number of votes for successful blob resolution",
+            &["blob_hash"]
+        );
+    BLOBS_FINALITY_VOTING_FAILURE: IntCounterVec
+        = register_int_counter_vec!(
+            "blobs_finality_voting_failure",
+            "Blobs finality: number of votes for failed blob resolution",
+            &["blob_hash"]
+        );
+    BLOBS_FINALITY_PENDING_BLOBS: IntGauge
+        = register_int_gauge!(
+            "blobs_finality_pending_blobs",
+            "Blobs finality: current count of pending blobs"
+        );
+    BLOBS_FINALITY_PENDING_BYTES: IntGauge
+        = register_int_gauge!("blobs_finality_pending_bytes", "Blobs finality: current count of pending bytes");
+}
+
+impl_traceables!(
+    TraceLevel::Info,
+    "IrohResolver",
+    BlobsFinalityVotingFailure,
+    BlobsFinalityVotingSuccess,
+    BlobsFinalityPendingBlobs,
+    BlobsFinalityPendingBytes
+);
+
+#[derive(Debug)]
+pub struct BlobsFinalityVotingSuccess {
+    pub blob_hash: Option<[u8; 32]>,
+}
+
+impl Recordable for BlobsFinalityVotingSuccess {
+    fn record_metrics(&self) {
+        BLOBS_FINALITY_VOTING_SUCCESS
+            .with_label_values(&[hex::encode(self.blob_hash.unwrap_or([0u8; 32])).as_str()])
+            .inc();
+    }
+}
+
+#[derive(Debug)]
+pub struct BlobsFinalityVotingFailure {
+    pub blob_hash: Option<[u8; 32]>,
+}
+
+impl Recordable for BlobsFinalityVotingFailure {
+    fn record_metrics(&self) {
+        BLOBS_FINALITY_VOTING_FAILURE
+            .with_label_values(&[hex::encode(self.blob_hash.unwrap_or([0u8; 32])).as_str()])
+            .inc();
+    }
+}
+
+#[derive(Debug)]
+pub struct BlobsFinalityPendingBlobs {
+    pub count: u64,
+}
+
+impl Recordable for BlobsFinalityPendingBlobs {
+    fn record_metrics(&self) {
+        BLOBS_FINALITY_PENDING_BLOBS.set(self.count as i64);
+    }
+}
+
+#[derive(Debug)]
+pub struct BlobsFinalityPendingBytes {
+    pub bytes_count: u64,
+}
+
+impl Recordable for BlobsFinalityPendingBytes {
+    fn record_metrics(&self) {
+        BLOBS_FINALITY_PENDING_BYTES.set(self.bytes_count as i64);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ipc_observability::emit;
+
+    #[test]
+    fn test_metrics() {
+        let registry = Registry::new();
+        register_metrics(&registry).unwrap();
+    }
+
+    #[test]
+    fn test_metric_increase() {
+        let registry = Registry::new();
+        register_metrics(&registry).unwrap();
+
+        emit(BlobsFinalityPendingBlobs { count: 1 });
+        emit(BlobsFinalityPendingBytes { bytes_count: 1 });
+    }
+
+    #[test]
+    fn test_emit() {
+        emit(BlobsFinalityVotingSuccess {
+            blob_hash: Some([0u8; 32]),
+        });
+
+        emit(BlobsFinalityVotingFailure {
+            blob_hash: Some([0u8; 32]),
+        });
+
+        emit(BlobsFinalityPendingBlobs { count: 1 });
+
+        emit(BlobsFinalityPendingBytes { bytes_count: 1 });
+    }
+}
