1a3bdd42d353ae69d2b2cd65857edd7091214641
Karel Moravec <moravec.wdd@gmail.com>
2024-11-14 14:26:25 +0100

    fix: address comments

--- fendermint/app/src/validators.rs
+++ fendermint/app/src/validators.rs
@@ -5,66 +5,32 @@
 
 use anyhow::anyhow;
 use fendermint_crypto::PublicKey;
-use fvm_shared::clock::ChainEpoch;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
-use tendermint::block::Height;
-use tendermint_rpc::{Client, Paging};
 
 #[derive(Clone)]
-pub(crate) struct ValidatorTracker<C> {
-    client: C,
+pub(crate) struct ValidatorTracker {
     public_keys: Arc<RwLock<HashMap<tendermint::account::Id, PublicKey>>>,
 }
 
-impl<C: Client> ValidatorTracker<C> {
-    pub fn new(client: C) -> Self {
+impl ValidatorTracker {
+    pub fn new() -> Self {
         Self {
-            client,
             public_keys: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 }
 
-impl<C: Client + Sync> ValidatorTracker<C> {
+impl ValidatorTracker {
     /// Get the public key of the validator by id. Note that the id is expected to be a validator.
-    pub async fn get_validator(
-        &self,
-        id: &tendermint::account::Id,
-        height: ChainEpoch,
-    ) -> anyhow::Result<PublicKey> {
-        if let Some(key) = self.get_from_cache(id) {
-            return Ok(key);
-        }
-
-        // this means validators have changed, re-pull all validators
-        let height = Height::try_from(height)?;
-        let response = self.client.validators(height, Paging::All).await?;
-
-        let mut new_validators = HashMap::new();
-        let mut pubkey = None;
-        for validator in response.validators {
-            let key = fendermint_pub_key_from_tendermint_pub_key(&validator.pub_key)?;
-
-            if *id == validator.address {
-                pubkey = Some(key);
-            }
-
-            new_validators.insert(validator.address, key);
-        }
-
-        *self.public_keys.write().unwrap() = new_validators;
-
-        // cannot find the validator, this should not have happened usually
-        pubkey.ok_or_else(|| anyhow!("{} not validator", id))
-    }
-
-    fn get_from_cache(&self, id: &tendermint::account::Id) -> Option<PublicKey> {
+    pub fn get_validator(&self, id: &tendermint::account::Id) -> anyhow::Result<PublicKey> {
         let keys = self.public_keys.read().unwrap();
-        keys.get(id).copied()
+        keys.get(id)
+            .copied()
+            .ok_or_else(|| anyhow!("validator not found: {:?}", id))
     }
 
-    pub fn hydrate_cache(
+    pub fn set_validators(
         &self,
         public_keys: Vec<(tendermint::account::Id, tendermint::public_key::PublicKey)>,
     ) {
@@ -78,7 +44,6 @@ impl<C: Client + Sync> ValidatorTracker<C> {
     }
 }
 
-// TODO Karel - consider using From trait
 fn fendermint_pub_key_from_tendermint_pub_key(
     key: &tendermint::public_key::PublicKey,
 ) -> anyhow::Result<PublicKey> {
