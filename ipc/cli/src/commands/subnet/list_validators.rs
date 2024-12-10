8f128686d97b879829bf2a2dd6c6a468ea4f7521
raulk <raul.kripalani@gmail.com>
2024-12-10 16:16:59 +0000

    clarify command description.

--- ipc/cli/src/commands/subnet/list_validators.rs
+++ ipc/cli/src/commands/subnet/list_validators.rs
@@ -34,7 +34,7 @@ impl CommandLineHandler for ListValidators {
 #[derive(Debug, Args)]
 #[command(
     name = "list validators",
-    about = "List the info of all the validators in the subnet"
+    about = "List the info of all the validators in the subnet, as viewed by the parent"
 )]
 pub(crate) struct ListValidatorsArgs {
     #[arg(long, help = "The target subnet to perform query")]
