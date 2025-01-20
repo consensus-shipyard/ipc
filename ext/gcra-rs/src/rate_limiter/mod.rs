3b61ed6bb2ae2502ad08a413e88d15b29d890b04
cryptoAtwill <willes.lau@protocol.ai>
2025-01-20 17:28:38 +0800

    headers

--- ext/gcra-rs/src/rate_limiter/mod.rs
+++
@@ -1,7 +0,0 @@
-mod clock;
-mod entry;
-#[allow(clippy::module_inception)]
-mod rate_limiter;
-
-pub use entry::*;
-pub use rate_limiter::*;
--- ext/libp2p-bitswap/src/query.rs
+++ ext/libp2p-bitswap/src/query.rs
@@ -1,3 +1,10 @@
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: MIT
+//
+// Forked from https://github.com/consensus-shipyard/libp2p-bitswap with assumed MIT license
+// as per Cargo.toml: https://github.com/consensus-shipyard/libp2p-bitswap/blob/7dd9cececda3e4a8f6e14c200a4b457159d8db33/Cargo.toml#L7
+//
+// License headers added post-fork.
 use crate::stats::{REQUESTS_TOTAL, REQUEST_DURATION_SECONDS};
 use fnv::{FnvHashMap, FnvHashSet};
 use libipld::Cid;
