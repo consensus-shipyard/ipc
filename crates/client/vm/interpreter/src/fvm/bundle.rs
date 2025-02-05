5327d6d19be17ba490ab157fbda7b382f4d66c1f
Bernhard Schuster <bernhard@ahoi.io>
2025-02-05 15:27:20 +0100

    clippy

--- crates/client/vm/interpreter/src/fvm/bundle.rs
+++ crates/client/vm/interpreter/src/fvm/bundle.rs
@@ -20,34 +20,31 @@ fn workspace_dir() -> PathBuf {
 
 /// Path to the Solidity contracts, indended to be used in tests.
 pub fn contracts_path() -> PathBuf {
-    let contracts_path = std::env::var("FM_CONTRACTS_DIR")
+    std::env::var("FM_CONTRACTS_DIR")
         .map(std::path::PathBuf::from)
-        .unwrap_or_else(|_| workspace_dir().join("contracts").join("out"));
-
-    contracts_path
+        .unwrap_or_else(|_| workspace_dir().join("contracts").join("out"))
 }
+
 /// Path to the builtin-actor bundle, indended to be used in tests.
 pub fn bundle_path() -> PathBuf {
-    let bundle_path = std::env::var("FM_BUILTIN_ACTORS_BUNDLE")
+    std::env::var("FM_BUILTIN_ACTORS_BUNDLE")
         .map(std::path::PathBuf::from)
         .unwrap_or_else(|_| {
             workspace_dir()
                 .join("crates")
-                .join("client/builtin-actors/output/bundle.car")
-        });
-
-    bundle_path
+                .join("client/builtin-actors/output")
+                .join("bundle.car")
+        })
 }
 
 /// Path to the in-repo custom actor bundle, intended to be used in tests.
 pub fn custom_actors_bundle_path() -> PathBuf {
-    let custom_actors_bundle_path = std::env::var("FM_CUSTOM_ACTORS_BUNDLE")
+    std::env::var("FM_CUSTOM_ACTORS_BUNDLE")
         .map(std::path::PathBuf::from)
         .unwrap_or_else(|_| {
             workspace_dir()
                 .join("crates")
-                .join("client/actors/output/custom_actors_bundle.car")
-        });
-
-    custom_actors_bundle_path
+                .join("client/actors/output")
+                .join("custom_actors_bundle.car")
+        })
 }
