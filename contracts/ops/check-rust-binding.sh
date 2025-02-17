c0151305cc9740db1e7633cb8d90053282160763
Bernhard Schuster <bernhard@ahoi.io>
2025-02-17 13:55:39 +0100

    undo

--- contracts/ops/check-rust-binding.sh
+++ contracts/ops/check-rust-binding.sh
@@ -3,10 +3,10 @@ set -eu
 set -o pipefail
 
 # checks if there are changes in rust binding
-if [[ `git status ../contract-bindings --porcelain` ]]; then
+if [[ `git status ../contracts/binding --porcelain` ]]; then
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     echo "********** NOT ALL RUST BINDINGS COMMITTED, COMMIT THEM **********\n";
-    git status ../contract-bindings --porcelain
+    git status ../contracts/binding --porcelain
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
     exit 1;
 fi;
