241ddf57a9b157ecdf0cd0348cc002c397ca5740
Bernhard Schuster <bernhard@ahoi.io>
2025-02-27 12:36:58 +0100

    include path fixup

--- fendermint/actors-custom-car/src/lib.rs
+++ fendermint/actors-custom-car/src/lib.rs
@@ -5,4 +5,4 @@ mod manifest;
 pub use manifest::Manifest;
 
 /// Included bytes for custom actor bundle, ~1.3M in size
-pub const CUSTOM_ACTORS: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "custom_actors_bundle.car"));
+pub const CUSTOM_ACTORS: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"),"/../actors/output/", "custom_actors_bundle.car"));
