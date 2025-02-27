7d2a09a1682c3aa994bc23c994c1bb769e26bb0a
Bernhard Schuster <bernhard@ahoi.io>
2025-02-27 12:53:48 +0100

    y

--- fendermint/actors-builtin-car/src/lib.rs
+++ fendermint/actors-builtin-car/src/lib.rs
@@ -1,2 +1,4 @@
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
 /// Included builtin actors bundle
 pub const BUILTIN_ACTORS_CAR: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/", "builtin_actors.car"));
