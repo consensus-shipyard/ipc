3b79c9d452839d4bfc492488d95c1f6ea4b7a677
Karel Moravec <moravec.wdd@gmail.com>
2025-03-25 15:34:39 +0100

    feat: add contracts deployer (#1318)
    Co-authored-by: cryptoAtwill <108330426+cryptoAtwill@users.noreply.github.com>
    Co-authored-by: LePremierHomme <57456510+LePremierHomme@users.noreply.github.com>

--- fendermint/vm/interpreter/src/genesis.rs
+++ fendermint/vm/interpreter/src/genesis.rs
@@ -14,10 +14,10 @@ use cid::Cid;
 use ethers::abi::Tokenize;
 use ethers::core::types as et;
 use fendermint_actor_eam::PermissionModeParams;
+use fendermint_eth_deployer::utils as deployer_utils;
 use fendermint_eth_hardhat::{ContractSourceAndName, Hardhat, FQN};
 use fendermint_vm_actor_interface::diamond::{EthContract, EthContractMap};
 use fendermint_vm_actor_interface::eam::EthAddress;
-use fendermint_vm_actor_interface::ipc::IPC_CONTRACTS;
 use fendermint_vm_actor_interface::{
     account, activity, burntfunds, chainmetadata, cron, eam, gas_market, init, ipc, reward, system,
     EMPTY_ARR,
@@ -274,7 +274,8 @@ impl<'a> GenesisBuilder<'a> {
         // STAGE 0: Declare the built-in EVM contracts we'll have to deploy.
         // ipc_entrypoints contains the external user facing contracts
         // all_ipc_contracts contains ipc_entrypoints + util contracts
-        let (all_ipc_contracts, ipc_entrypoints) = self.collect_contracts()?;
+        let (all_ipc_contracts, ipc_entrypoints) =
+            deployer_utils::collect_contracts(&self.hardhat)?;
 
         // STAGE 1: First we initialize native built-in actors.
         // System actor
@@ -486,34 +487,6 @@ impl<'a> GenesisBuilder<'a> {
 
         Ok(out)
     }
-
-    fn collect_contracts(&self) -> anyhow::Result<(Vec<ContractSourceAndName>, EthContractMap)> {
-        let mut all_contracts = Vec::new();
-        let mut top_level_contracts = EthContractMap::default();
-
-        top_level_contracts.extend(IPC_CONTRACTS.clone());
-
-        all_contracts.extend(top_level_contracts.keys());
-        all_contracts.extend(
-            top_level_contracts
-                .values()
-                .flat_map(|c| c.facets.iter().map(|f| f.name)),
-        );
-        // Collect dependencies of the main IPC actors.
-        let mut eth_libs = self
-            .hardhat
-            .dependencies(
-                &all_contracts
-                    .iter()
-                    .map(|n| (contract_src(n), *n))
-                    .collect::<Vec<_>>(),
-            )
-            .context("failed to collect EVM contract dependencies")?;
-
-        // Only keep library dependencies, not contracts with constructors.
-        eth_libs.retain(|(_, d)| !top_level_contracts.contains_key(d.as_str()));
-        Ok((eth_libs, top_level_contracts))
-    }
 }
 
 // Configuration for deploying IPC contracts.
@@ -610,10 +583,6 @@ fn deploy_contracts(
     Ok(())
 }
 
-fn contract_src(name: &str) -> PathBuf {
-    PathBuf::from(format!("{name}.sol"))
-}
-
 struct ContractDeployer<'a, DB> {
     hardhat: &'a Hardhat,
     top_contracts: &'a EthContractMap,
@@ -645,13 +614,13 @@ where
     ) -> anyhow::Result<()> {
         let fqn = self.hardhat.fqn(lib_src.as_ref(), lib_name);
 
-        let bytecode = self
+        let artifact = self
             .hardhat
-            .bytecode(&lib_src, lib_name, &self.lib_addrs)
+            .prepare_deployment_artifact(&lib_src, lib_name, &self.lib_addrs)
             .with_context(|| format!("failed to load library bytecode {fqn}"))?;
 
         let eth_addr = state
-            .create_evm_actor(*next_id, bytecode)
+            .create_evm_actor(*next_id, artifact.bytecode)
             .with_context(|| format!("failed to create library actor {fqn}"))?;
 
         let id_addr = et::Address::from(EthAddress::from_id(*next_id).0);
@@ -686,15 +655,20 @@ where
     {
         let contract = self.top_contract(contract_name)?;
         let contract_id = contract.actor_id;
-        let contract_src = contract_src(contract_name);
+        let contract_src = deployer_utils::contract_src(contract_name);
 
-        let bytecode = self
+        let artifact = self
             .hardhat
-            .bytecode(contract_src, contract_name, &self.lib_addrs)
+            .prepare_deployment_artifact(contract_src, contract_name, &self.lib_addrs)
             .with_context(|| format!("failed to load {contract_name} bytecode"))?;
 
         let eth_addr = state
-            .create_evm_actor_with_cons(contract_id, &contract.abi, bytecode, constructor_params)
+            .create_evm_actor_with_cons(
+                contract_id,
+                &contract.abi,
+                artifact.bytecode,
+                constructor_params,
+            )
             .with_context(|| format!("failed to create {contract_name} actor"))?;
 
         let id_addr = et::Address::from(EthAddress::from_id(contract_id).0);
@@ -714,36 +688,12 @@ where
 
     /// Collect Facet Cuts for the diamond pattern, where the facet address comes from already deployed library facets.
     fn facets(&self, contract_name: &str) -> anyhow::Result<Vec<FacetCut>> {
-        let contract = self.top_contract(contract_name)?;
-        let mut facet_cuts = Vec::new();
-
-        for facet in contract.facets.iter() {
-            let facet_name = facet.name;
-            let facet_src = contract_src(facet_name);
-            let facet_fqn = self.hardhat.fqn(&facet_src, facet_name);
-
-            let facet_addr = self
-                .lib_addrs
-                .get(&facet_fqn)
-                .ok_or_else(|| anyhow!("facet {facet_name} has not been deployed"))?;
-
-            let method_sigs = facet
-                .abi
-                .functions()
-                .filter(|f| f.signature() != "init(bytes)")
-                .map(|f| f.short_signature())
-                .collect();
-
-            let facet_cut = FacetCut {
-                facet_address: *facet_addr,
-                action: 0, // Add
-                function_selectors: method_sigs,
-            };
-
-            facet_cuts.push(facet_cut);
-        }
-
-        Ok(facet_cuts)
+        deployer_utils::collect_facets(
+            contract_name,
+            self.hardhat,
+            self.top_contracts,
+            &self.lib_addrs,
+        )
     }
 
     fn top_contract(&self, contract_name: &str) -> anyhow::Result<&EthContract> {
