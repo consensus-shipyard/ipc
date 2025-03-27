bbd4dddac3bc1bfa7a69f944b35faef9a38c7004
Bernhard Schuster <bernhard@ahoi.io>
2025-03-27 17:39:58 +0100

    tackle hardhat
    It's just a linker of solidity artifacts, nothing more nothing less.

---
+++ fendermint/eth/hardhat-builtin/src/lib.rs
@@ -0,0 +1,4 @@
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+pub const BUILTIN_SOL_CAR: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/xxx.car"));
