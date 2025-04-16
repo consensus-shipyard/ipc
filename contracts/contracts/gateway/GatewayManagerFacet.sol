373cd58b0975703e15961daf9f5e4fa47f1261f8
Bernhard Schuster <bernhard@ahoi.io>
2025-04-16 18:12:13 +0200

    y

--- contracts/contracts/gateway/GatewayManagerFacet.sol
+++ contracts/contracts/gateway/GatewayManagerFacet.sol
@@ -34,7 +34,7 @@ contract GatewayManagerFacet is GatewayActorModifiers, ReentrancyGuard {
     /// subnet from genesis (without having to wait for the subnet to be spawned to propagate the funds).
     function register(uint256 genesisCircSupply, uint256 collateral) external payable {
         // The supplied amount must align with the arguments
-        require(msg.value == genesisCircSupply + collateral, "Deposit amount mismatch");
+        require(msg.value == (genesisCircSupply + collateral), "Deposit amount mismatched");
 
         // If L2+ support is not enabled, only allow the registration of new
         // subnets in the root
