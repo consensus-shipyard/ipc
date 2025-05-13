043143b1c61eb8ea23cb5bf7acbd2e0fbc069f90
Bernhard Schuster <bernhard@ahoi.io>
2025-05-13 10:54:01 +0200

    license headers

--- ipc/wallet/src/fvm/keystore/json.rs
+++ ipc/wallet/src/fvm/keystore/json.rs
@@ -1,3 +1,5 @@
+// Copyright 2022-2024 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
 use crate::fvm::serialization::json::signature_type::SignatureTypeJson;
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 
