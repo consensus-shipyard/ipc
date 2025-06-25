5b8c1a3a6e2ea37c11bcd9b18e7580b816d6d63d
Karel Moravec <moravec.wdd@gmail.com>
2025-06-25 15:40:38 +0200

    feat: add cmd to init default config

--- fendermint/app/options/src/config.rs
+++ fendermint/app/options/src/config.rs
@@ -1,7 +1,24 @@
 // Copyright 2022-2024 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use clap::Args;
+use clap::{Args, Subcommand};
 
 #[derive(Args, Debug)]
-pub struct ConfigArgs;
+pub struct PrintConfigArgs;
+
+#[derive(Args, Debug)]
+pub struct InitConfigArgs;
+
+#[derive(Subcommand, Debug)]
+pub enum ConfigCommands {
+    /// Create a new default config file
+    Init(InitConfigArgs),
+    /// Print the current config
+    Print(PrintConfigArgs),
+}
+
+#[derive(Args, Debug)]
+pub struct ConfigArgs {
+    #[command(subcommand)]
+    pub command: ConfigCommands,
+}
--- fendermint/app/src/cmd/config.rs
+++ fendermint/app/src/cmd/config.rs
@@ -1,19 +1,63 @@
 // Copyright 2022-2024 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use fendermint_app_options::config::ConfigArgs;
+use fendermint_app_options::config::{ConfigArgs, ConfigCommands, InitConfigArgs, PrintConfigArgs};
 
-use crate::{cmd, settings::Settings};
+use crate::{
+    cmd, cmd::load_settings, options::Options, settings::utils::expand_tilde, settings::Settings,
+};
+use anyhow::Context;
+use std::{fs, path::Path, sync::Arc};
+use toml;
 
 cmd! {
-  ConfigArgs(self, settings) {
+  ConfigArgs(self, options: Arc<Options>) {
+    match &self.command {
+        ConfigCommands::Init(args) => args.exec(options).await,
+        ConfigCommands::Print(args) => args.exec(load_settings(options)?).await
+    }
+  }
+}
+
+cmd! {
+  PrintConfigArgs(self, settings) {
     print_settings(settings)
   }
 }
 
+cmd! {
+  InitConfigArgs(self, options: Arc<Options>) {
+    write_default_settings(options.config_dir())
+  }
+}
+
 fn print_settings(settings: Settings) -> anyhow::Result<()> {
-    // Currently the `Settings` doesn't support `Serialize`,
-    // but if it did we could choose a format to print in.
-    println!("{settings:?}");
+    let settings_str = toml::to_string_pretty(&settings)?;
+    println!("{}", settings_str);
+    Ok(())
+}
+
+pub fn write_default_settings(config_dir: impl AsRef<Path>) -> anyhow::Result<()> {
+    // make sure the folder exists
+    let dir = expand_tilde(config_dir);
+    fs::create_dir_all(&dir).with_context(|| {
+        format!(
+            "failed to create configuration directory `{}`",
+            dir.display()
+        )
+    })?;
+
+    // serialize the defaults
+    let settings = Settings::default();
+    let toml_str = toml::to_string_pretty(&settings)
+        .context("failed to serialize default Settings to TOML")?;
+
+    // write the file
+    let file = dir.join("default.toml");
+    println!("Writing default config to `{}`", file.display());
+
+    fs::write(&file, toml_str)
+        .with_context(|| format!("failed to write default config to `{}`", file.display()))?;
+
     Ok(())
 }
