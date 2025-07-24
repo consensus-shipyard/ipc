047c37b60c50010ee2cbab71e168c7fd78bf8230
cryptoAtwill <willes.lau@protocol.ai>
2025-07-24 20:29:43 +0800

    adding cometbft client

---
+++ ipc/provider/src/manager/cometbft.rs
@@ -0,0 +1,25 @@
+// Copyright 2022-2025 Protocol Labs
+// SPDX-License-Identifier: MIT
+
+use fvm_shared::clock::ChainEpoch;
+use tendermint::block::signed_header::SignedHeader;
+use tendermint_rpc::Client;
+
+pub struct CometbftClient {
+    client: tendermint_rpc::HttpClient,
+}
+
+impl CometbftClient {
+    pub fn new_from_url(url: &str) -> Self {
+        let client =
+            tendermint_rpc::HttpClient::new(url).expect("could not create tendermint client");
+        Self { client }
+    }
+
+    pub async fn fetch_signed_header(&self, height: ChainEpoch) -> anyhow::Result<SignedHeader> {
+        let h = tendermint::block::Height::from(height as u32);
+        let query_response = self.client.commit(h).await?;
+
+        Ok(query_response.signed_header)
+    }
+}
