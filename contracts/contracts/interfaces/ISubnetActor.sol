bcbac715f1e66b8e40d3141faef232b6cc8ca1e2
cryptoAtwill <108330426+cryptoAtwill@users.noreply.github.com>
2025-07-28 16:46:18 +0800

    feat(node): patch replay cometbft (#1405)
    Co-authored-by: cryptoAtwill <willes.lau@protocol.ai>

--- contracts/contracts/interfaces/ISubnetActor.sol
+++ contracts/contracts/interfaces/ISubnetActor.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.23;
 
 import {Asset} from "../structs/Subnet.sol";
+import {ValidatorInfo} from "../structs/Subnet.sol";
 
 /// @title Subnet actor interface
 interface ISubnetActor {
@@ -10,7 +11,10 @@ interface ISubnetActor {
     /// @notice Returns the total amount of confirmed collateral across all validators.
     function getTotalCurrentPower() external view returns (uint256);
 
-    /// @notice Checks if the validator address is in an active state.
-    /// @param validator The address of the checked validator
-    function getCurrentPower(address validator) external view returns (uint256);
+    /// @notice Obtain the active validator address by its position index in the validator list array.
+    function getActiveValidatorAddressByIndex(uint256 index) external view returns (address);
+
+    /// @notice Returns detailed information about a specific validator.
+    /// @param validatorAddress The address of the validator to query information for.
+    function getValidator(address validatorAddress) external view returns (ValidatorInfo memory validator);
 }
---
+++ ipc/provider/src/manager/cometbft/mod.rs
@@ -0,0 +1,27 @@
+// Copyright 2022-2025 Protocol Labs
+// SPDX-License-Identifier: MIT
+
+use std::fmt::{Display, Formatter};
+use fvm_shared::clock::ChainEpoch;
+use tendermint_rpc::Client;
+
+pub struct CometbftClient {
+    client: tendermint_rpc::HttpClient,
+}
+
+pub struct SignedHeader(tendermint::block::signed_header::SignedHeader);
+
+impl CometbftClient {
+    pub fn new_from_url(url: &str) -> Self {
+        let client =
+            tendermint_rpc::HttpClient::new(url).expect("could not create tendermint client");
+        Self { client }
+    }
+
+    pub async fn fetch_signed_header(&self, height: ChainEpoch) -> anyhow::Result<SignedHeader> {
+        let h = tendermint::block::Height::from(height as u32);
+        let query_response = self.client.commit(h).await?;
+
+        Ok(SignedHeader(query_response.signed_header))
+    }
+}
