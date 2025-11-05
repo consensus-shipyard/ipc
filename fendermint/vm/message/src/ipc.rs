f4272fb5a72d3e6a214f54f06e25fa809525599b
cryptoAtwill <willes.lau@protocol.ai>
2025-11-05 21:03:37 +0800

    fix imports

--- fendermint/vm/message/src/ipc.rs
+++ fendermint/vm/message/src/ipc.rs
@@ -1,9 +1,14 @@
 // Copyright 2022-2024 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use cid::Cid;
 use fendermint_actor_blobs_shared::blobs::SubscriptionId;
-
-use fvm_shared::clock::ChainEpoch;
+use fvm_shared::{
+    address::Address, clock::ChainEpoch, crypto::signature::Signature, econ::TokenAmount, MethodNum,
+};
+use ipc_api::subnet_id::SubnetID;
+use iroh_base::NodeId;
+use iroh_blobs::Hash;
 use serde::{Deserialize, Serialize};
 
 /// Messages involved in InterPlanetary Consensus.
@@ -14,20 +19,20 @@ pub enum IpcMessage {
     /// state that to be checked and voted by validators.
     TopDownExec(ParentFinality),
 
-    /// Proposed by validators at the credit debit interval set at genesis.
-    DebitCreditAccounts,
+    // /// Proposed by validators at the credit debit interval set at genesis.
+    // DebitCreditAccounts,
 
-    /// List of blobs that needs to be enqueued for resolution.
-    BlobPending(PendingBlob),
+    // /// List of blobs that needs to be enqueued for resolution.
+    // BlobPending(PendingBlob),
 
-    /// Proposed by validators when a blob has been finalized and is ready to be executed.
-    BlobFinalized(FinalizedBlob),
+    // /// Proposed by validators when a blob has been finalized and is ready to be executed.
+    // BlobFinalized(FinalizedBlob),
 
-    /// Proposed by validators when a read request has been enqueued for resolution.
-    ReadRequestPending(PendingReadRequest),
+    // /// Proposed by validators when a read request has been enqueued for resolution.
+    // ReadRequestPending(PendingReadRequest),
 
-    /// Proposed by validators when a read request has been closed.
-    ReadRequestClosed(ClosedReadRequest),
+    // /// Proposed by validators when a read request has been closed.
+    // ReadRequestClosed(ClosedReadRequest),
 }
 
 /// A proposal of the parent view that validators will be voting on.
